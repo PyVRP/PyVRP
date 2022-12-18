a9fe96fe753bf9fbd0a24f0b8afa66db0d8c3aa2
Niels Wouda <nielswouda@gmail.com>
2022-12-18 22:30:42 +0100

    Two tests for time warp calculations

--- hgs/test/test_TimeWindowSegment.cpp
+++ hgs/test/test_TimeWindowSegment.cpp
@@ -1,5 +1,33 @@
 #include <gtest/gtest.h>
 
+#include "Matrix.h"
 #include "TimeWindowSegment.h"
 
-// TODO
+TEST(TestTimeWindowSegment, segmentTimeWarp)
+{
+    Matrix<int> mat(0);
+
+    // 0 time warp passed in, so we expect 0 time warp.
+    TimeWindowSegment tws1 = {&mat, 0, 0, 0, 0, 0, 0, 0};
+    ASSERT_EQ(tws1.segmentTimeWarp(), 0);
+
+    // 5 time warp passed in, so we expect 5 time warp.
+    TimeWindowSegment tws2 = {&mat, 0, 0, 0, 5, 0, 0, 0};
+    ASSERT_EQ(tws2.segmentTimeWarp(), 5);
+}
+
+TEST(TestTimeWindowSegment, totalTimeWarp)
+{
+    Matrix<int> mat(0);
+
+    // 5 time warp passed in, so we expect 5 time warp.
+    TimeWindowSegment tws1 = {&mat, 0, 0, 0, 5, 0, 0, 0};
+    ASSERT_EQ(tws1.totalTimeWarp(), 5);
+
+    // 5 time warp passed in, together with 0 twLate and 5 release: we expect
+    // 5 segment time warp, and 10 total time warp (due to release time).
+    TimeWindowSegment tws2 = {&mat, 0, 0, 0, 5, 0, 0, 5};
+    ASSERT_EQ(tws2.totalTimeWarp(), 10);
+}
+
+// TODO test merge
