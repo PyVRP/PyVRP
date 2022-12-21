5070207cc5b87904bb489d1d64fe3a5c52f6b96b
Niels Wouda <nielswouda@gmail.com>
2022-12-21 18:38:16 +0100

    Reorganise Params tests a bit, for clarity

--- hgs/test/test_Params.cpp
+++ hgs/test/test_Params.cpp
@@ -3,94 +3,88 @@
 #include "Config.h"
 #include "Params.h"
 
-TEST(ParamsFromFileTest, dimension)
-{
-    Config const config;
-    auto const path = "data/ORTEC-VRPTW-ASYM-4c69f727-d1-n204-k12.txt";
-    auto const params = Params::fromFile(config, path);
+/**
+ *  THE FIRST SET OF TESTS CHECK WHETHER INVALID FILES ARE PROPERLY REJECTED.
+ **/
 
-    ASSERT_EQ(params.nbClients, 204);  // n204
-    ASSERT_EQ(params.nbVehicles, 12);  // k12
-}
-
-TEST(ParamsFromFileTest, throwsUnknownEdgeWeightFmt)
+TEST(ParamsFromFileThrowsTest, UnknownEdgeWeightFmt)
 {
     Config const config;
     auto const path = "data/UnknownEdgeWeightFmt.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsUnknownEdgeWeightType)
+TEST(ParamsFromFileThrowsTest, UnknownEdgeWeightType)
 {
     Config const config;
     auto const path = "data/UnknownEdgeWeightType.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsUnknownFile)
+TEST(ParamsFromFileThrowsTest, UnknownFile)
 {
     Config const config;
     auto const path = "somewhere that does not exist";
     ASSERT_THROW(Params::fromFile(config, path), std::invalid_argument);
 }
 
-TEST(ParamsFromFileTest, throwsUnknownSection)
+TEST(ParamsFromFileThrowsTest, UnknownSectionInFile)
 {
     Config const config;
     auto const path = "data/FileWithUnknownSection.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsWrongIdDepot)
+TEST(ParamsFromFileThrowsTest, WrongIdDepot)
 {
     Config const config;
     auto const path = "data/DepotNotOne.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsWrongDepotEndIdentifier)
+TEST(ParamsFromFileThrowsTest, WrongDepotEndIdentifier)
 {
     Config const config;
     auto const path = "data/DepotSectionDoesNotEndInMinusOne.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsMoreThanOneDepot)
+TEST(ParamsFromFileThrowsTest, MoreThanOneDepot)
 {
     Config const config;
     auto const path = "data/MoreThanOneDepot.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsNonZeroDepotServiceDuration)
+TEST(ParamsFromFileThrowsTest, NonZeroDepotServiceDuration)
 {
     Config const config;
     auto const path = "data/NonZeroDepotServiceDuration.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsNonZeroDepotReleaseTime)
+TEST(ParamsFromFileThrowsTest, NonZeroDepotReleaseTime)
 {
     Config const config;
     auto const path = "data/NonZeroDepotReleaseTime.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsNonZeroDepotOpenTimeWindow)
+TEST(ParamsFromFileThrowsTest, NonZeroDepotOpenTimeWindow)
 {
     Config const config;
     auto const path = "data/NonZeroDepotOpenTimeWindow.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsNonZeroDepotDemand)
+TEST(ParamsFromFileThrowsTest, NonZeroDepotDemand)
 {
     Config const config;
     auto const path = "data/NonZeroDepotDemand.txt";
     ASSERT_THROW(Params::fromFile(config, path), std::runtime_error);
 }
 
-TEST(ParamsFromFileTest, throwsInconsistentTimeWindows)
+TEST(ParamsFromFileThrowsTest, InconsistentTimeWindows)
 {
     Config const config;
     auto const earlyEqLate = "data/TimeWindowOpenEqualToClose.txt";
@@ -99,4 +93,19 @@ TEST(ParamsFromFileTest, throwsInconsistentTimeWindows)
     // Params::fromFile should throw when any twEarly >= twLate
     ASSERT_THROW(Params::fromFile(config, earlyEqLate), std::runtime_error);
     ASSERT_THROW(Params::fromFile(config, earlyGtLate), std::runtime_error);
+}
+
+/**
+ * HERE START TESTS THAT CHECK CONTENT, NOT JUST WHETHER INVALID FILES ARE
+ * PROPERLY REJECTED.
+ **/
+
+TEST(ParamsFromFileContentTest, dimension)
+{
+    Config const config;
+    auto const path = "data/ORTEC-VRPTW-ASYM-4c69f727-d1-n204-k12.txt";
+    auto const params = Params::fromFile(config, path);
+
+    ASSERT_EQ(params.nbClients, 204);  // n204
+    ASSERT_EQ(params.nbVehicles, 12);  // k12
 }
