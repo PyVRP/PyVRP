bbe343cfe9c307d5e9110e18e1c9a6d0e70cb600
l-lan <l.lan@vu.nl>
2023-01-16 08:33:51 +0100

    Remove nodiscard from TWS

--- hgs/src/TimeWindowSegment.cpp
+++ hgs/src/TimeWindowSegment.cpp
@@ -1,11 +1,8 @@
 #include "TimeWindowSegment.h"
 
-[[nodiscard]] int TimeWindowSegment::segmentTimeWarp() const
-{
-    return timeWarp;
-}
+int TimeWindowSegment::segmentTimeWarp() const { return timeWarp; }
 
-[[nodiscard]] int TimeWindowSegment::totalTimeWarp() const
+int TimeWindowSegment::totalTimeWarp() const
 {
     return segmentTimeWarp() + std::max(release - twLate, 0);
 }
