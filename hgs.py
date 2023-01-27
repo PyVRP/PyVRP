2c9a7193c09075667288913c78884debbd430320
Niels Wouda <N-Wouda@users.noreply.github.com>
2023-01-27 11:25:04 +0100

    Performance improvements (#45)
    * Inline performance-critical methods
    
    * Speed up implementation by inlining a few more methods
    
    * Update hgs.py to function as a benchmark script
    
    * Use cached feasibility member

--- hgs.py
+++ hgs.py
@@ -4,7 +4,14 @@ import argparse
 import glob
 import importlib.machinery
 import importlib.util
+import pathlib
+import re
+from functools import partial
 from time import perf_counter
+from typing import Optional
+
+import numpy as np
+from tqdm.contrib.concurrent import process_map
 
 
 def get_hgspy(where: str):
@@ -17,33 +24,89 @@ def get_hgspy(where: str):
     return hgspy
 
 
+def name2size(name: str) -> int:
+    """
+    Extracts the instance size (i.e., num clients) from the instance name.
+    """
+    return int(re.search(r'-n(\d{1,3})-', name).group(1))
+
+
+def static_time_limit(n_clients: int, phase: str) -> int:
+    """
+    Returns the time limit (in seconds) for solving the static problem for
+    the passed-in number of clients and competition phase.
+
+    Instances are grouped into categories of <300/300-500/>500 customers.
+    - 3/5/8 minutes for the quali(fication) phase
+    - 5/10/15 minutes for the final phase
+    """
+    if phase not in ["quali", "final"]:
+        raise ValueError(f"Invalid phase: {phase}")
+
+    if n_clients < 300:
+        return 180 if phase == "quali" else 300
+    elif 300 <= n_clients <= 500:
+        return 300 if phase == "quali" else 600
+    else:
+        return 480 if phase == "quali" else 900
+
+
+def tabulate(headers, rows) -> str:
+    # These lengths are used to space each column properly.
+    lengths = [len(header) for header in headers]
+
+    for row in rows:
+        for idx, cell in enumerate(row):
+            lengths[idx] = max(lengths[idx], len(str(cell)))
+
+    header = [
+        "  ".join(f"{h:<{l}s}" for l, h in zip(lengths, headers)),
+        "  ".join("-" * l for l in lengths),
+    ]
+
+    content = ["  ".join(f"{str(c):>{l}s}"
+                         for l, c in zip(lengths, row))
+               for row in rows]
+
+    return "\n".join(header + content)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(prog="hgs")
 
-    parser.add_argument("data_loc")
-    parser.add_argument("res_loc")
-    parser.add_argument("seed", type=int)
+    parser.add_argument("--seed", type=int, required=True)
+    parser.add_argument("--num_procs", type=int, default=4)
     parser.add_argument("--debug", action="store_true")
-
-    # TODO config
+    parser.add_argument(
+        "--instance_pattern", default="instances/ORTEC-VRPTW-ASYM-*.txt"
+    )
 
     stop = parser.add_mutually_exclusive_group(required=True)
+    stop.add_argument("--phase", choices=["quali", "final"])
     stop.add_argument("--max_runtime", type=float)
     stop.add_argument("--max_iterations", type=int)
 
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
+def solve(
+        data_loc: str,
+        seed: int,
+        debug: bool,
+        phase: Optional[str],
+        max_runtime: Optional[float],
+        max_iterations: Optional[int],
+        **kwargs,
+):
+    where = pathlib.Path(data_loc)
 
-    if args.debug:
+    if debug:
         hgspy = get_hgspy("debug/lib/hgspy*.so")
     else:
         hgspy = get_hgspy("release/lib/hgspy*.so")
 
-    data = hgspy.ProblemData.from_file(args.data_loc)
-    rng = hgspy.XorShift128(seed=args.seed)
+    data = hgspy.ProblemData.from_file(str(where))
+    rng = hgspy.XorShift128(seed=seed)
     pen_manager = hgspy.PenaltyManager(data.vehicle_capacity())
     diversity = hgspy.diversity.broken_pairs_distance
     pop = hgspy.Population(data, pen_manager, rng, diversity)
@@ -63,8 +126,8 @@ def main():
         ls.add_node_operator(op)
 
     route_ops = [
-        hgspy.operators.RelocateStar(data, pen_manager),
-        hgspy.operators.SwapStar(data, pen_manager),
+        # hgspy.operators.RelocateStar(data, pen_manager),
+        # hgspy.operators.SwapStar(data, pen_manager),
     ]
 
     for op in route_ops:
@@ -73,17 +136,70 @@ def main():
     crossover = hgspy.crossover.selective_route_exchange
     algo = hgspy.GeneticAlgorithm(data, pen_manager, rng, pop, ls, crossover)
 
-    if args.max_runtime is not None:
-        stop = hgspy.stop.MaxRuntime(args.max_runtime)
+    if phase is not None:
+        t_lim = static_time_limit(name2size(data_loc), phase)
+        stop = hgspy.stop.MaxRuntime(t_lim)
+    elif max_runtime is not None:
+        stop = hgspy.stop.MaxRuntime(max_runtime)
     else:
-        stop = hgspy.stop.MaxIterations(args.max_iterations)
+        stop = hgspy.stop.MaxIterations(max_iterations)
 
     start = perf_counter()
+
     res = algo.run(stop)
+    best = res.get_best_found()
+
     run_time = round(perf_counter() - start, 2)
 
-    best = res.get_best_found()
-    best.to_file(args.res_loc, run_time)
+    return (
+        where.stem,
+        "Y" if best.is_feasible() else "N",
+        int(best.cost()),
+        res.get_iterations(),
+        round(run_time, 3),
+    )
+
+
+def main():
+    args = parse_args()
+
+    func = partial(solve, **vars(args))
+    func_args = sorted(glob.glob(args.instance_pattern), key=name2size)
+
+    tqdm_kwargs = dict(max_workers=args.num_procs, unit="instance")
+    data = process_map(func, func_args, **tqdm_kwargs)
+
+    dtypes = [
+        ("inst", "U37"),
+        ("ok", "U1"),
+        ("obj", int),
+        ("iters", int),
+        ("time", float),
+    ]
+
+    data = np.asarray(data, dtype=dtypes)
+
+    headers = [
+        "Instance",
+        "OK",
+        "Objective",
+        "Iters. (#)",
+        "Time (s)",
+    ]
+
+    table = tabulate(headers, data)
+
+    print("\n", table, "\n", sep="")
+
+    obj_all = data["obj"]
+    obj_feas = data[data["ok"] == "Y"]["obj"]
+
+    print(f"      Avg. objective: {obj_all.mean():.0f}", end=" ")
+    print(f"(w/o infeas: {obj_feas.mean():.0f})" if obj_feas.size > 0 else "")
+
+    print(f"     Avg. iterations: {data['iters'].mean():.0f}")
+    print(f"   Avg. run-time (s): {data['time'].mean():.2f}")
+    print(f"        Total not OK: {np.count_nonzero(data['ok'] == 'N')}")
 
 
 if __name__ == "__main__":
--- hgs/src/Route.cpp
+++ hgs/src/Route.cpp
@@ -56,39 +56,6 @@ void Route::setupRouteTimeWindows()
     } while (!node->isDepot());
 }
 
-Node *Route::operator[](size_t position) const
-{
-    assert(position > 0);
-    return nodes[position - 1];
-}
-
-bool Route::isFeasible() const
-{
-    return !hasExcessCapacity() && !hasTimeWarp();
-}
-
-bool Route::hasExcessCapacity() const
-{
-    return static_cast<size_t>(load()) > data->vehicleCapacity();
-}
-
-bool Route::hasTimeWarp() const { return timeWarp() > 0; }
-
-int Route::load() const { return nodes.back()->cumulatedLoad; }
-
-int Route::timeWarp() const
-{
-    auto const &tw = nodes.back()->twBefore;
-    return tw.totalTimeWarp();
-}
-
-bool Route::empty() const { return size() == 0; }
-
-size_t Route::size() const
-{
-    return nodes.size() - 1;  // exclude end depot
-}
-
 void Route::update()
 {
     auto const oldNodes = nodes;
@@ -133,6 +100,12 @@ void Route::update()
 
     setupAngle();
     setupRouteTimeWindows();
+
+    load_ = nodes.back()->cumulatedLoad;
+    isLoadFeasible_ = static_cast<size_t>(load_) <= data->vehicleCapacity();
+
+    timeWarp_ = nodes.back()->twBefore.totalTimeWarp();
+    isTimeWarpFeasible_ = timeWarp_ == 0;
 }
 
 std::ostream &operator<<(std::ostream &out, Route const &route)
