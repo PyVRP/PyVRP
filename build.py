349190506159687de2ddc9c5d18b004e388cc238
Niels Wouda <nielswouda@gmail.com>
2023-01-28 14:18:43 +0100

    Build via skbuild/poetry

---
+++ build.py
@@ -0,0 +1,13 @@
+import skbuild
+
+
+def build(kwargs):
+    skbuild.setup(
+        cmake_source_dir="hgs",
+        cmake_install_dir="build",
+        cmake_install_target="hgspy",
+        cmake_args=[
+            "-G=Unix Makefiles",  # TODO switch to Ninja?
+        ],
+        **kwargs
+    )
