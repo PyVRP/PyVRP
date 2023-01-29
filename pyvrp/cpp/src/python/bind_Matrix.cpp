8499d4a4d82872f3e35aa0de3f08aedfa0576686
Niels Wouda <nielswouda@gmail.com>
2023-01-29 16:15:01 +0100

    Clang-format bind_Matrix.cpp

--- pyvrp/cpp/src/python/bind_Matrix.cpp
+++ pyvrp/cpp/src/python/bind_Matrix.cpp
@@ -10,11 +10,13 @@ void bind_Matrix(py::module_ &m)
         .def(py::init<size_t, size_t>(), py::arg("n_rows"), py::arg("n_cols"))
         .def(py::init<std::vector<std::vector<int>>>(), py::arg("data"))
         .def("__getitem__",
-             [](Matrix<int> &m, std::pair<size_t, size_t> idx) -> int
-             { return m(idx.first, idx.second); })
+             [](Matrix<int> &m, std::pair<size_t, size_t> idx) -> int {
+                 return m(idx.first, idx.second);
+             })
         .def("__setitem__",
-             [](Matrix<int> &m, std::pair<size_t, size_t> idx, int value)
-             { m(idx.first, idx.second) = value; })
+             [](Matrix<int> &m, std::pair<size_t, size_t> idx, int value) {
+                 m(idx.first, idx.second) = value;
+             })
         .def("max", &Matrix<int>::max)
         .def("size", &Matrix<int>::size);
 
@@ -23,11 +25,13 @@ void bind_Matrix(py::module_ &m)
         .def(py::init<size_t, size_t>(), py::arg("n_rows"), py::arg("n_cols"))
         .def(py::init<std::vector<std::vector<double>>>(), py::arg("data"))
         .def("__getitem__",
-             [](Matrix<double> &m, std::pair<size_t, size_t> idx) -> double
-             { return m(idx.first, idx.second); })
+             [](Matrix<double> &m, std::pair<size_t, size_t> idx) -> double {
+                 return m(idx.first, idx.second);
+             })
         .def("__setitem__",
-             [](Matrix<double> &m, std::pair<size_t, size_t> idx, double value)
-             { m(idx.first, idx.second) = value; })
+             [](Matrix<double> &m,
+                std::pair<size_t, size_t> idx,
+                double value) { m(idx.first, idx.second) = value; })
         .def("max", &Matrix<double>::max)
         .def("size", &Matrix<double>::size);
 }
