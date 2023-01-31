6cf104cd51d7f359369eab202c24b50035030c32
Niels Wouda <nielswouda@gmail.com>
2023-01-31 21:47:37 +0100

    Test Population.Params argument validation

--- pyvrp/tests/test_Population.py
+++ pyvrp/tests/test_Population.py
@@ -1,1 +1,87 @@
-# TODO
+from numpy.testing import assert_raises
+from pytest import mark
+
+from pyvrp import Population
+
+
+@mark.parametrize(
+    "min_pop_size,"
+    "generation_size,"
+    "nb_elite,"
+    "nb_close,"
+    "lb_diversity,"
+    "ub_diversity",
+    [
+        (0, 1, 1, 1, 0.0, 1.0),  # zero min_pop_size
+        (-1, 1, 1, 1, 0.0, 1.0),  # -1 min_pop_size
+        (1, -1, 1, 1, 0.0, 1.0),  # -1 generation_size
+        (1, 1, -1, 1, 0.0, 1.0),  # -1 nb_elite
+        (1, 1, 1, -1, 0.0, 1.0),  # -1 nb_close
+        (1, 1, 1, -1, -1, 1.0),  # -1 lb_diversity
+        (1, 1, 1, -1, 2, 1.0),  # 2 lb_diversity
+        (1, 1, 1, -1, 0, -1.0),  # -1 ub_diversity
+        (1, 1, 1, -1, 0, 2.0),  # 2 ub_diversity
+        (1, 1, 1, -1, 1, 0.5),  # ub_diversity < lb_diversity
+        (1, 1, 1, -1, 0.5, 0.5),  # ub_diversity == lb_diversity
+    ],
+)
+def test_params_constructor_throws_when_arguments_invalid(
+    min_pop_size: int,
+    generation_size: int,
+    nb_elite: int,
+    nb_close: int,
+    lb_diversity: float,
+    ub_diversity: float,
+):
+    """
+    Tests that invalid configurations are not accepted.
+    """
+    with assert_raises(ValueError):
+        Population.Params(
+            min_pop_size,
+            generation_size,
+            nb_elite,
+            nb_close,
+            lb_diversity,
+            ub_diversity,
+        )
+
+
+@mark.parametrize(
+    "min_pop_size,"
+    "generation_size,"
+    "nb_elite,"
+    "nb_close,"
+    "lb_diversity,"
+    "ub_diversity",
+    [
+        (1, 1, 1, 1, 0.0, 0.5),  # >0 min_pop_size
+        (1, 0, 1, 1, 0.0, 0.5),  # 0 generation_size
+        (1, 1, 0, 1, 0.0, 0.5),  # 0 nb_elite
+        (1, 1, 1, 0, 0.0, 0.5),  # 0 nb_close
+        (1, 1, 1, 1, 0.0, 0.5),  # 0 lb_diversity
+        (1, 1, 1, 1, 0.0, 1.0),  # 1 ub_diversity
+    ],
+)
+def test_params_constructor_does_not_raise_when_arguments_valid(
+    min_pop_size: int,
+    generation_size: int,
+    nb_elite: int,
+    nb_close: int,
+    lb_diversity: float,
+    ub_diversity: float,
+):
+    """
+    Tests valid boundary cases.
+    """
+    Population.Params(
+        min_pop_size,
+        generation_size,
+        nb_elite,
+        nb_close,
+        lb_diversity,
+        ub_diversity,
+    )
+
+
+# TODO functional tests
