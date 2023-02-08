15bab4f85ec2b62ad4a0d5bbbdcbfc6c5e31e123
Niels Wouda <nielswouda@gmail.com>
2023-02-08 10:20:55 +0100

    Document ProblemData

--- pyvrp/ProblemData.pyi
+++ pyvrp/ProblemData.pyi
@@ -1,21 +1,83 @@
 from typing import Any, List, Tuple
 
 class Client:
-    def __init__(self, *args, **kwargs) -> None: ...
+    """
+    Simple data object storing all client data as properties.
+    """
+
     @property
-    def demand(self) -> int: ...
+    def demand(self) -> int:
+        """
+        Returns this client's demand.
+
+        Returns
+        -------
+        int
+            The amount this client's demanding.
+        """
     @property
-    def release_time(self) -> int: ...
+    def release_time(self) -> int:
+        """
+        Returns this client's release time.
+
+        Returns
+        -------
+        int
+            The earliest time this client can leave the depot.
+        """
     @property
-    def serv_dur(self) -> int: ...
+    def service_duration(self) -> int:
+        """
+        Returns this client's service duration.
+
+        Returns
+        -------
+        int
+            This client's service duration, that is, the amount of time we need
+            to visit the client for.
+        """
     @property
-    def tw_early(self) -> int: ...
+    def tw_early(self) -> int:
+        """
+        Returns this client's opening time window.
+
+        Returns
+        -------
+        int
+            Earliest time at which we can visit this client.
+        """
     @property
-    def tw_late(self) -> int: ...
+    def tw_late(self) -> int:
+        """
+        Returns this client's closing time window.
+
+        Returns
+        -------
+        int
+            Latest time at which we can visit this client.
+        """
     @property
-    def x(self) -> int: ...
+    def x(self) -> int:
+        """
+        Horizontal coordinate of this client, that is, the 'x' part of the
+        client's (x, y) location tuple.
+
+        Returns
+        -------
+        int
+            Client's location.
+        """
     @property
-    def y(self) -> int: ...
+    def y(self) -> int:
+        """
+        Vertical coordinate of this client, that is, the 'y' part of the
+        client's (x, y) location tuple.
+
+        Returns
+        -------
+        int
+            Client's location.
+        """
 
 class ProblemData:
     def __init__(
@@ -28,7 +90,44 @@ class ProblemData:
         service_durations: List[int],
         duration_matrix: List[List[int]],
         release_times: List[int],
-    ) -> None: ...
+    ):
+        """
+        Creates a problem data instance. This instance contains all information
+        needed to solve the vehicle routing problem.
+
+        Parameters
+        ----------
+        coords
+            Array of (x, y) coordinates. The first coordinate at index 0 is
+            assumed to be the depot.
+        demands
+            Array of client demands. The demand at index 0 is assumed to be the
+            depot's demand, and should be zero.
+        nb_vehicles
+            The number of vehicles in this problem instance.
+        vehicle_cap
+            Homogenous vehicle capacity for all vehicles in the problem
+            instance.
+        time_windows
+            Array of (early, late) time windows. The time window at index 0 is
+            assumed to be the depot's time window, and describes the overall
+            time horizon.
+        service_durations
+            Array of service durations, that is, the length of time needed to
+            service a customer upon visiting. The service duration at index 0
+            is assumed to be the depot's service time, and should be zero.
+        duration_matrix
+            A matrix that gives the travel times between clients (and the
+            depot at index 0). Does not have to be symmetric.
+        release_times
+            Release times give the earliest time at which a client's request
+            may leave the depot.
+
+        Notes
+        -----
+        All array data assume that the data at or involving index 0 relates to
+        the depot, and all other indices specify client information.
+        """
     def client(self, client: int) -> Client:
         """
         Returns client data for the given client.
@@ -53,10 +152,38 @@ class ProblemData:
         Client
             A simple data object containing the depot's information.
         """
-    def dist(self, first: int, second: int) -> int: ...
-    def distance_matrix(self, *args, **kwargs) -> Any: ...
+    def dist(self, first: int, second: int) -> int:
+        """
+        Returns the travel duration between the first and second argument,
+        according to this instance's travel duration matrix.
+
+        Parameters
+        ----------
+        first
+            Client or depot number.
+        second
+            Client or depot number.
+
+        Returns
+        -------
+        int
+            Travel duration between the given clients.
+        """
+    def distance_matrix(self) -> Any:
+        """
+        Returns the travel duration matrix used for distance/duration
+        computations.
+
+        Returns
+        -------
+        Any
+            Travel duration matrix.
+        """
     @staticmethod
-    def from_file(where: str) -> ProblemData: ...
+    def from_file(where: str) -> ProblemData:
+        """
+        TODO remove
+        """
     @property
     def num_clients(self) -> int:
         """
@@ -77,7 +204,6 @@ class ProblemData:
         int
             Number of vehicles in the instance.
         """
-        pass
     @property
     def vehicle_capacity(self) -> int:
         """
@@ -89,4 +215,3 @@ class ProblemData:
         int
             Capacity of each vehicle in the instance.
         """
-        pass
