f59e796ff744e0b5cd84e6682b5890ccc6f2a71c
l-lan <l.lan@vu.nl>
2023-02-11 09:19:04 +0100

    Remove old tests, add involved tests

--- pyvrp/crossover/tests/test_selective_route_exchange.py
+++ pyvrp/crossover/tests/test_selective_route_exchange.py
@@ -5,35 +5,17 @@ from pyvrp.crossover import selective_route_exchange as srex
 from pyvrp.tests.helpers import read
 
 
-def test_srex_identical_individuals():
+def test_srex_move_all_routes():
     """
-    Tests if crossing two identical individuals yields an identical offspring.
-    """
-    data = read("data/OkSmall.txt")
-    pm = PenaltyManager(data.vehicle_capacity)
-    rng = XorShift128(seed=42)  # nMovedroutes = 2
-
-    indiv1 = Individual(data, pm, [[1, 3], [4], [2]])
-    indiv2 = Individual(data, pm, [[1, 3], [4], [2]])
-    parents = (indiv1, indiv2)
-
-    offspring = srex(parents, data, pm, rng)
-
-    assert_equal(offspring.get_routes(), indiv1.get_routes())
-    assert_equal(offspring.get_routes(), indiv2.get_routes())
-
-
-def test_srex_offspring_is_second_parent_when_all_routes_moved():
-    """
-    Tests if all routes are moved during crossover, then SREX produces an
+    Tests if all routes are moved during crossover, then SREX produces
     an offspring that is identical to the second parent.
     """
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
-    rng = XorShift128(seed=4)  # results in nMovedRoutes = 3
+    rng = XorShift128(seed=4)  # nMovedRoutes = 3
 
     indiv1 = Individual(data, pm, [[1], [2], [3, 4]])
-    indiv2 = Individual(data, pm, [[1, 3], [4], [2]])
+    indiv2 = Individual(data, pm, [[1, 2], [3], [4]])
     parents = (indiv1, indiv2)
 
     offspring = srex(parents, data, pm, rng)
@@ -41,42 +23,50 @@ def test_srex_offspring_is_second_parent_when_all_routes_moved():
     assert_equal(offspring.get_routes(), indiv2.get_routes())
 
 
-def test_srex_identical_single_route_individuals():
+def test_srex_move_start_indinces():
     """
-    Tests that crossing two identical individuals with a single route
-    yields the same route.
-    """
-    data = read("data/OkSmall.txt")
-    pm = PenaltyManager(data.vehicle_capacity)
-    rng = XorShift128(seed=42)  # results in nMovedRoutes = 1
-
-    indiv1 = Individual(data, pm, [[1, 2, 3, 4], [], []])
-    indiv2 = Individual(data, pm, [[], [], [1, 2, 3, 4]])
-    parents = (indiv1, indiv2)
-
-    # Indiv2's route [2, 3, 4] will be moved. This results in two
-    # candidate offspring individuals: [[1], [2, 3, 4]] and
-    offspring = srex(parents, data, pm, rng)
-
-    assert_equal(offspring.get_routes(), indiv2.get_routes())
-
-
-def test_srex_move_one_route():
-    """
-    Tests that crossing two identical individuals with a single route
-    yields the same route.
+    Tests the case where the initial start indices are changed.
     """
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
-    rng = XorShift128(seed=0)  # results in startA = 1 and nMovedRoutes = 1
+    rng = XorShift128(seed=2)  # startA = 0 and nMovedRoutes = 1
 
-    indiv1 = Individual(data, pm, [[1], [2, 3], [4]])
-    indiv2 = Individual(data, pm, [[4], [3, 2], [1]])
+    indiv1 = Individual(data, pm, [[1, 2, 3], [4], []])
+    indiv2 = Individual(data, pm, [[1, 2, 4], [3], []])
     parents = (indiv1, indiv2)
 
-    # The search for which routes to move starts at the middle routes. Moving
-    # right or left does not lead to route replacements that have less
-    # differences, so the middle routes will be moved.
+    # The start indices at initialization are startA = 0 and startB = 0.
+    # The difference for A to move left (= right) is -1. The difference for B
+    # to move left (= right) is 0. The new indices become startA = 1 and
+    # startB = 0. There are no improving moves in this position since the
+    # difference for A is 1 and difference for B is 1.
+    # So, indiv1's route [4] will be replaced by indiv2's route [1, 2, 4].
+    # This results in two candidate offspring, [[3], [1, 2, 4]] with cost
+    # 10195, and [[1, 2, 3], [4]] with cost 31029. The first candidate is
+    # returned, because it has the lowest cost
     offspring = srex(parents, data, pm, rng)
 
-    assert_equal(offspring.get_routes(), [[1], [3, 2], [4]])
+    assert_equal(offspring.get_routes(), [[3], [1, 2, 4], []])
+
+
+def test_srex_greedy_repair():
+    """
+    Tests the case where greedy repair is used during SREX crossover.
+    """
+    data = read("data/OkSmall.txt")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=2)  # startA = 0 and nMovedRoutes = 1
+
+    indiv1 = Individual(data, pm, [[1, 2], [3, 4], []])
+    indiv2 = Individual(data, pm, [[2, 3], [4, 1], []])
+    parents = (indiv1, indiv2)
+
+    # The start indices do not change because there are no improving moves.
+    # So, indiv1's route [1, 2] will be replaced by indiv2's route [2, 3].
+    # This results in two incomplete offspring, [[2, 3], [4]] and [[2], [3, 4]]
+    # which are both repaired by greedy repair. After repair, we obtain the
+    # offspring [[2, 3, 1], [4]] with cost 8735, and [[1, 2], [3, 4]] with
+    # cost 9725.
+    offspring = srex(parents, data, pm, rng)
+
+    assert_equal(offspring.get_routes(), [[2, 3, 1], [4], []])
