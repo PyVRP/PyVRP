09f2a6e595806893750e7dde1405a66ea1b30a49
Niels Wouda <nielswouda@gmail.com>
2023-02-12 18:01:15 +0100

    Unified commandline interface

--- pyvrp/__init__.py
+++ pyvrp/__init__.py
@@ -3,5 +3,6 @@ from .Individual import Individual
 from .PenaltyManager import PenaltyManager, PenaltyParams
 from .Population import Population, PopulationParams
 from .ProblemData import ProblemData
+from .Result import Result
 from .XorShift128 import XorShift128
 from .read import read
