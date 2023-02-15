8aa8ea7fad13c9828c4230a56b92203f100fe219
Niels Wouda <nielswouda@gmail.com>
2023-02-15 14:41:00 +0100

    Add test for relocate using empty routes

--- pyvrp/educate/tests/test_Exchange.py
+++ pyvrp/educate/tests/test_Exchange.py
@@ -2,15 +2,37 @@ from numpy.testing import assert_, assert_equal
 from pytest import mark
 
 from pyvrp import Individual, PenaltyManager, XorShift128
-from pyvrp.educate import Exchange11, Exchange22, Exchange33, LocalSearch
+from pyvrp.educate import LocalSearch
+from pyvrp.educate.Exchange import (
+    Exchange10,
+    Exchange11,
+    Exchange20,
+    Exchange21,
+    Exchange22,
+    Exchange30,
+    Exchange31,
+    Exchange32,
+    Exchange33,
+)
 from pyvrp.tests.helpers import read
 
 
-@mark.parametrize("operator", [Exchange11, Exchange22, Exchange33])
+@mark.parametrize(
+    "operator",
+    [
+        Exchange11,
+        Exchange21,
+        Exchange22,
+        Exchange31,
+        Exchange32,
+        Exchange33,
+    ],
+)
 def test_swap_single_route_stays_single_route(operator):
     """
-    Swap operators on a single route can only move within the same route, so
-    they can never find a solution that has more than one route.
+    Swap operators (operators with non-zero M) on a single route can only move
+    within the same route, so they can never find a solution that has more than
+    one route.
     """
     data = read("data/RC208.txt", "solomon", "dimacs")
     pm = PenaltyManager(data.vehicle_capacity)
@@ -28,4 +50,37 @@ def test_swap_single_route_stays_single_route(operator):
 
     # The new solution should strictly improve on our original solution.
     assert_equal(individual.num_routes(), 1)
+    assert_(curr_cost > individual.cost())
+
+
+@mark.parametrize(
+    "operator",
+    [
+        Exchange10,
+        Exchange20,
+        Exchange30,
+    ],
+)
+def test_relocate_uses_empty_routes(operator):
+    """
+    Unlike the swapping exchange operators, relocate should be able to relocate
+    clients to empty routes if that is an improvement.
+    """
+    data = read("data/RC208.txt", "solomon", "dimacs")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=42)
+
+    ls = LocalSearch(data, pm, rng)
+    op = operator(data, pm)
+    ls.add_node_operator(op)
+
+    single_route = list(range(1, data.num_clients + 1))
+    individual = Individual(data, pm, [single_route])
+    curr_cost = individual.cost()
+
+    ls.search(individual)
+
+    # The new solution should strictly improve on our original solution, and
+    # should use more routes.
+    assert_(individual.num_routes() > 1)
     assert_(curr_cost > individual.cost())
