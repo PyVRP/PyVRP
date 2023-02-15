6a0e1d5016fa737672dfc4c27962e5fdb7520f33
Niels Wouda <nielswouda@gmail.com>
2023-02-15 15:56:36 +0100

    Test exchange with overlapping depots

--- pyvrp/educate/tests/test_Exchange.py
+++ pyvrp/educate/tests/test_Exchange.py
@@ -2,7 +2,7 @@ from numpy.testing import assert_, assert_equal
 from pytest import mark
 
 from pyvrp import Individual, PenaltyManager, XorShift128
-from pyvrp.educate import LocalSearch
+from pyvrp.educate import LocalSearch, LocalSearchParams
 from pyvrp.educate.Exchange import (
     Exchange10,
     Exchange11,
@@ -38,19 +38,20 @@ def test_swap_single_route_stays_single_route(operator):
     pm = PenaltyManager(data.vehicle_capacity)
     rng = XorShift128(seed=42)
 
-    ls = LocalSearch(data, pm, rng)
+    params = LocalSearchParams(nb_granular=data.num_clients)
+    ls = LocalSearch(data, pm, rng, params)
     op = operator(data, pm)
     ls.add_node_operator(op)
 
     single_route = list(range(1, data.num_clients + 1))
     individual = Individual(data, pm, [single_route])
-    curr_cost = individual.cost()
+    copy = Individual(individual)
 
     ls.search(individual)
 
     # The new solution should strictly improve on our original solution.
     assert_equal(individual.num_routes(), 1)
-    assert_(curr_cost > individual.cost())
+    assert_(individual.cost() < copy.cost())
 
 
 @mark.parametrize(
@@ -70,17 +71,52 @@ def test_relocate_uses_empty_routes(operator):
     pm = PenaltyManager(data.vehicle_capacity)
     rng = XorShift128(seed=42)
 
-    ls = LocalSearch(data, pm, rng)
+    params = LocalSearchParams(nb_granular=data.num_clients)
+    ls = LocalSearch(data, pm, rng, params)
     op = operator(data, pm)
     ls.add_node_operator(op)
 
     single_route = list(range(1, data.num_clients + 1))
     individual = Individual(data, pm, [single_route])
-    curr_cost = individual.cost()
+    copy = Individual(individual)
 
     ls.search(individual)
 
     # The new solution should strictly improve on our original solution, and
     # should use more routes.
     assert_(individual.num_routes() > 1)
-    assert_(curr_cost > individual.cost())
+    assert_(individual.cost() < copy.cost())
+
+
+@mark.parametrize(
+    "operator",
+    [
+        Exchange22,
+        Exchange30,
+        Exchange31,
+        Exchange32,
+        Exchange33,
+    ],
+)
+def test_cannot_exchange_when_parts_overlap_with_depot(operator):
+    """
+    (N, M)-exchange works by exchanging N nodes starting at some node U with
+    M nodes starting at some node V. But when there is no sequence of N or M
+    nodes that does not contain the depot (because the routes are very short),
+    then no exchange is possible.
+    """
+    data = read("data/OkSmall.txt")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=42)
+
+    params = LocalSearchParams(nb_granular=data.num_clients)
+    ls = LocalSearch(data, pm, rng, params)
+    op = operator(data, pm)
+    ls.add_node_operator(op)
+
+    individual = Individual(data, pm, [[1, 2], [3], [4]])
+    copy = Individual(individual)
+
+    ls.search(individual)
+
+    assert_equal(individual, copy)
