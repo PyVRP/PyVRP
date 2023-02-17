f3b9a9986e6cc127a227f8a785f745089ea38d14
Wouter Kool <1150455+wouterkool@users.noreply.github.com>
2023-02-17 14:55:06 +0100

    Remove redundant test case

--- pyvrp/educate/tests/test_LocalSearch.py
+++ pyvrp/educate/tests/test_LocalSearch.py
@@ -8,7 +8,6 @@ from pyvrp.educate import LocalSearchParams
     [
         1,  # non-empty neighbourhood structure (nbGranular)
         0,  # zero post processing should be OK
-        5,  # no weights for wait time or time warp should be OK
     ],
 )
 def test_local_search_params_does_not_raise_for_valid_arguments(
