ce631414ffdb1fe194b5a8da8dc2c76d81d424ff
Niels Wouda <nielswouda@gmail.com>
2023-02-17 12:38:32 +0100

    Add tests for 2OPT

--- pyvrp/educate/tests/test_MoveTwoClientsReversed.py
+++ pyvrp/educate/tests/test_MoveTwoClientsReversed.py
@@ -1,4 +1,5 @@
 from numpy.testing import assert_, assert_equal
+from pytest import mark
 
 from pyvrp import Individual, PenaltyManager, XorShift128
 from pyvrp.educate import (
@@ -12,7 +13,7 @@ from pyvrp.tests.helpers import read
 def test_single_route_OkSmall():
     """
     This test checks that MoveTwoClientsReversed properly solves the small
-    instance where we know what's going on.
+    instance where we know what is going on.
     """
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
@@ -35,9 +36,30 @@ def test_single_route_OkSmall():
     # (2, 3) was inserted after the depot as (3, 2)
     assert_equal(individual.get_routes(), [[3, 2, 1, 4], [], []])
 
-    # These alternative, two-route solution are all reachable from the returned
-    # solution. And they can be created by MoveTwoClientsReversed. So they must
-    # have a cost that's at best equal to the returned solution's cost.
+    # These two-route solutions can all be created by MoveTwoClientsReversed
+    # from the returned solution. So they must have a cost that's at best equal
+    # to the returned solution's cost.
     for routes in ([[3, 2], [4, 1]], [[2, 3], [1, 4]], [[3, 4], [1, 2]]):
         other = Individual(data, pm, routes)
         assert_(individual.cost() <= other.cost())
+
+
+@mark.parametrize("seed", [2643, 2742, 2941, 3457, 4299, 4497, 6178, 6434])
+def test_RC208_instance(seed: int):
+    data = read("data/RC208.txt", "solomon", "dimacs")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=seed)
+
+    params = LocalSearchParams(nb_granular=data.num_clients)
+    ls = LocalSearch(data, pm, rng, params)
+    op = MoveTwoClientsReversed(data, pm)
+    ls.add_node_operator(op)
+
+    single_route = list(range(1, data.num_clients + 1))
+    individual = Individual(data, pm, [single_route])
+    copy = Individual(individual)
+
+    ls.search(individual)
+
+    # The new solution should strictly improve on our original solution.
+    assert_(individual.cost() < copy.cost())
