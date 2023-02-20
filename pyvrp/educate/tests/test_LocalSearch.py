a4b7a563a6db7002756a9150caaed854c414b9ac
Niels Wouda <nielswouda@gmail.com>
2023-02-20 17:20:11 +0100

    Move tests

--- pyvrp/educate/tests/test_LocalSearch.py
+++ pyvrp/educate/tests/test_LocalSearch.py
@@ -1,7 +1,8 @@
-from numpy.testing import assert_raises
+from numpy.testing import assert_, assert_equal, assert_raises
+from pytest import mark
 
 from pyvrp import Individual, PenaltyManager, XorShift128
-from pyvrp.educate import LocalSearch
+from pyvrp.educate import LocalSearch, NeighbourhoodParams, compute_neighbours
 from pyvrp.tests.helpers import read
 
 
@@ -32,3 +33,57 @@ def test_local_search_raises_when_neighbourhood_structure_is_empty():
 
     with assert_raises(RuntimeError):
         ls.search(individual)
+
+
+@mark.parametrize(
+    "weight_wait_time,"
+    "weight_time_warp,"
+    "nb_granular,"
+    "symmetric_proximity,"
+    "symmetric_neighbours",
+    [
+        (20, 20, 10, True, False),
+        (20, 20, 10, True, True),
+        # From original c++ implementation
+        # (18, 20, 34, False),
+        (18, 20, 34, True, True),
+    ],
+)
+def test_local_search_set_get_neighbours(
+    weight_wait_time: int,
+    weight_time_warp: int,
+    nb_granular: int,
+    symmetric_proximity: bool,
+    symmetric_neighbours: bool,
+):
+    data = read("data/RC208.txt", "solomon", round_func="trunc")
+
+    seed = 42
+    rng = XorShift128(seed=seed)
+    pen_manager = PenaltyManager(data.vehicle_capacity)
+    ls = LocalSearch(data, pen_manager, rng)
+
+    params = NeighbourhoodParams(
+        weight_wait_time,
+        weight_time_warp,
+        nb_granular,
+        symmetric_proximity,
+        symmetric_neighbours,
+    )
+    neighbours = compute_neighbours(data, params)
+
+    # Test that before we set neighbours we don't have same
+    assert_(ls.get_neighbours() != neighbours)
+
+    # Test after we set we have the same
+    ls.set_neighbours(neighbours)
+    ls_neighbours = ls.get_neighbours()
+    assert_equal(ls_neighbours, neighbours)
+
+    # Check that the bindings make a copy (in both directions)
+    assert_(ls_neighbours is not neighbours)
+    ls_neighbours[1] = []
+    assert_(ls.get_neighbours() != ls_neighbours)
+    assert_equal(ls.get_neighbours(), neighbours)
+    neighbours[1] = []
+    assert_(ls.get_neighbours() != neighbours)
--- pyvrp/educate/tests/test_neighbourhood.py
+++ pyvrp/educate/tests/test_neighbourhood.py
@@ -3,8 +3,7 @@ from typing import List
 from numpy.testing import assert_, assert_equal, assert_raises
 from pytest import mark
 
-from pyvrp import PenaltyManager, XorShift128
-from pyvrp.educate import LocalSearch, NeighbourhoodParams, compute_neighbours
+from pyvrp.educate import NeighbourhoodParams, compute_neighbours
 from pyvrp.tests.helpers import read
 
 
@@ -15,10 +14,11 @@ from pyvrp.tests.helpers import read
     "symmetric_proximity,"
     "symmetric_neighbours",
     [
-        (20, 20, 0, True, False),  # empty neighbourhood structure (nbGranular)
+        # empty neighbourhood structure (nb_granular == 0)
+        (20, 20, 0, True, False),
     ],
 )
-def test_local_search_params_raises_for_invalid_arguments(
+def test_neighbourhood_params_raises_for_invalid_arguments(
     weight_wait_time: int,
     weight_time_warp: int,
     nb_granular: int,
@@ -42,13 +42,13 @@ def test_local_search_params_raises_for_invalid_arguments(
     "symmetric_proximity,"
     "symmetric_neighbours",
     [
-        # non-empty neighbourhood structure (nbGranular)
+        # non-empty neighbourhood structure (nb_granular > 0)
         (20, 20, 1, True, False),
         # no weights for wait time or time warp should be OK
         (0, 0, 1, True, False),
     ],
 )
-def test_local_search_params_does_not_raise_for_valid_arguments(
+def test_neighbourhood_params_does_not_raise_for_valid_arguments(
     weight_wait_time: int,
     weight_time_warp: int,
     nb_granular: int,
@@ -172,56 +172,3 @@ def test_more_neighbours_than_instance_size():
 
     for neighb in neighbours[1:]:
         assert_equal(len(neighb), data.num_clients - 1)
-
-
-@mark.parametrize(
-    "weight_wait_time,"
-    "weight_time_warp,"
-    "nb_granular,"
-    "symmetric_proximity,"
-    "symmetric_neighbours",
-    [
-        (20, 20, 10, True, False),
-        (20, 20, 10, True, True),
-        # From original c++ implementation
-        # (18, 20, 34, False),
-        (18, 20, 34, True, True),
-    ],
-)
-def test_set_get_neighbours(
-    weight_wait_time: int,
-    weight_time_warp: int,
-    nb_granular: int,
-    symmetric_proximity: bool,
-    symmetric_neighbours: bool,
-):
-    data = read("data/RC208.txt", "solomon", round_func="trunc")
-
-    seed = 42
-    rng = XorShift128(seed=seed)
-    pen_manager = PenaltyManager(data.vehicle_capacity)
-    ls = LocalSearch(data, pen_manager, rng)
-
-    params = NeighbourhoodParams(
-        weight_wait_time,
-        weight_time_warp,
-        nb_granular,
-        symmetric_proximity,
-        symmetric_neighbours,
-    )
-    neighbours = compute_neighbours(data, params)
-    # neighbours = compute_neighbours(data)
-    # Test that before we set neighbours we don't have same
-    assert_(ls.get_neighbours() != neighbours)
-    # Test after we set we have the same
-    ls.set_neighbours(neighbours)
-    ls_neighbours = ls.get_neighbours()
-    assert_equal(ls_neighbours, neighbours)
-
-    # Check that the bindings make a copy (in both directions)
-    assert_(ls_neighbours is not neighbours)
-    ls_neighbours[1] = []
-    assert_(ls.get_neighbours() != ls_neighbours)
-    assert_equal(ls.get_neighbours(), neighbours)
-    neighbours[1] = []
-    assert_(ls.get_neighbours() != neighbours)
