cd3b3624d473cdb5ed91dec1e9aa3a5ee1943386
Niels Wouda <nielswouda@gmail.com>
2023-02-21 17:26:01 +0100

    Split steps in build script

--- build_extensions.py
+++ build_extensions.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import pathlib
 from subprocess import check_call
 from typing import List
@@ -25,7 +26,12 @@ def parse_args():
         help="Which type of solver to compile. Defaults to 'vrptw'.",
     )
     parser.add_argument(
-        "--regen_stubs",
+        "--clean",
+        action="store_true",
+        help="Clean build and installation directories before building.",
+    )
+    parser.add_argument(
+        "--regenerate_type_stubs",
         action="store_true",
         help="""
         Whether to regenerate the MyPy type stubs as well. Default False, since
@@ -42,18 +48,40 @@ def parse_args():
     return parser.parse_args()
 
 
+def clean(build_dir: pathlib.Path, install_dir: pathlib.Path):
+    check_call(["rm", "-rf", str(build_dir)])
+
+    for extension in install_dir.rglob("*.so"):
+        extension.unlink()
+
+    for extension in install_dir.rglob("*.pyd"):
+        extension.unlink()
+
+
+def regen_stubs(install_dir: pathlib.Path):
+    def _regen(extension):
+        ext_dir = extension.parent
+        ext_name, _ = extension.name.split(".", maxsplit=1)
+
+        check_call(
+            ["stubgen", "--parse-only", "-o", ".", "-m", ext_name],
+            cwd=ext_dir,
+        )
+
+    for extension in install_dir.rglob("*.so"):
+        _regen(extension)
+
+    for extension in install_dir.rglob("*.pyd"):
+        _regen(extension)
+
+
 def build(
-    build_dir: str,
+    build_dir: pathlib.Path,
     build_type: str,
     problem: str,
-    regen_stubs: bool,
     additional: List[str],
-    **kwargs,
 ):
     cwd = pathlib.Path.cwd()
-    build_loc = cwd / build_dir
-    install_loc = cwd / "pyvrp"
-
     args = [
         # fmt: off
         "--buildtype", build_type,
@@ -63,22 +91,28 @@ def build(
         # fmt: on
     ]
 
-    cmd = "configure" if build_loc.exists() else "setup"
-    check_call(["meson", cmd, build_loc, *args])
-    check_call(["meson", "compile", "-C", build_loc])
-    check_call(["meson", "install", "-C", build_loc])
+    cmd = "configure" if build_dir.exists() else "setup"
+    check_call(["meson", cmd, build_dir, *args])
+    check_call(["meson", "compile", "-C", build_dir])
+    check_call(["meson", "install", "-C", build_dir])
 
-    if regen_stubs:
-        for extension in install_loc.rglob("*.so"):  # TODO only *.so?
-            ext_dir = extension.parent
-            ext_name, _ = extension.name.split(".", maxsplit=1)
 
-            check_call(
-                ["stubgen", "--parse-only", "-o", ".", "-m", ext_name],
-                cwd=ext_dir,
-            )
+def main():
+    args = parse_args()
+    cwd = pathlib.Path.cwd()
+    build_dir = cwd / args.build_dir
+    install_dir = cwd / "pyvrp"
+
+    if args.clean or os.environ.get("CIBUILDWHEEL", "0") == "1":
+        # Always start from an entirely clean build when building wheels in
+        # the CI. Else only do so when expressly asked.
+        clean(build_dir, install_dir)
+
+    build(build_dir, args.build_type, args.problem, args.additional)
+
+    if args.regenerate_type_stubs:
+        regen_stubs(install_dir)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    build(**vars(args))
+    main()
