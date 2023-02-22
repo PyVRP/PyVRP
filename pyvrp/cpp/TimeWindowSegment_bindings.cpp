dfbb3ae66d8f7f248817b98152fd63c9dc93e5e7
Niels Wouda <nielswouda@gmail.com>
2023-02-22 23:37:02 +0100

    Remove release times

--- pyvrp/cpp/TimeWindowSegment_bindings.cpp
+++ pyvrp/cpp/TimeWindowSegment_bindings.cpp
@@ -8,16 +8,14 @@ using TWS = TimeWindowSegment;
 PYBIND11_MODULE(TimeWindowSegment, m)
 {
     py::class_<TWS>(m, "TimeWindowSegment")
-        .def(py::init<Matrix<int> const *, int, int, int, int, int, int, int>(),
+        .def(py::init<Matrix<int> const *, int, int, int, int, int, int>(),
              py::arg("dist"),
              py::arg("idx_first"),
              py::arg("idx_last"),
              py::arg("duration"),
              py::arg("time_warp"),
              py::arg("tw_early"),
-             py::arg("tw_late"),
-             py::arg("release"))
-        .def("segment_time_warp", &TWS::segmentTimeWarp)
+             py::arg("tw_late"))
         .def("total_time_warp", &TWS::totalTimeWarp)
         .def_static("merge", &TWS::merge<>)
         .def_static("merge", &TWS::merge<TWS>)
