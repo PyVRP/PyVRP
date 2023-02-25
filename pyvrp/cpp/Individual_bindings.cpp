f70efbd86763f8dd85fb9bb91f6ceefbd2c280c5
l-lan <l.lan@vu.nl>
2023-02-25 22:47:58 +0100

    clang-format

--- pyvrp/cpp/Individual_bindings.cpp
+++ pyvrp/cpp/Individual_bindings.cpp
@@ -29,19 +29,18 @@ PYBIND11_MODULE(_Individual, m)
         .def("has_excess_capacity", &Individual::hasExcessCapacity)
         .def("has_time_warp", &Individual::hasTimeWarp)
         .def("__eq__", &Individual::operator==)
-        .def("__copy__",
-             [](Individual const &individual)
-             { return Individual(individual); })
+        .def(
+            "__copy__",
+            [](Individual const &individual) { return Individual(individual); })
         .def(
             "__deepcopy__",
-            [](Individual const &individual, py::dict)
-            { return Individual(individual); },
+            [](Individual const &individual, py::dict) {
+                return Individual(individual);
+            },
             py::arg("memo"))
-        .def("__str__",
-             [](Individual const &individual)
-             {
-                 std::stringstream stream;
-                 stream << individual;
-                 return stream.str();
-             });
+        .def("__str__", [](Individual const &individual) {
+            std::stringstream stream;
+            stream << individual;
+            return stream.str();
+        });
 }
