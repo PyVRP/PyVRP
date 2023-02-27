2e9044e82d505086f84888df55075789e99c0b3d
Niels Wouda <nielswouda@gmail.com>
2023-02-27 12:16:42 +0100

    Test SWAP* on a number of different seeds

--- pyvrp/educate/tests/test_SwapStar.py
+++ pyvrp/educate/tests/test_SwapStar.py
@@ -1,4 +1,5 @@
-from numpy.testing import assert_
+from numpy.testing import assert_, assert_equal
+from pytest import mark
 
 from pyvrp import Individual, PenaltyManager, XorShift128
 from pyvrp.educate import (
@@ -11,12 +12,12 @@ from pyvrp.educate import (
 from pyvrp.tests.helpers import read
 
 
-def test_swap_star_identifies_more_moves_than_regular_swap():
+def test_swap_star_identifies_additional_moves_over_regular_swap():
     """
     SWAP* can move two clients to any position in the routes, whereas regular
     swap ((1, 1)-exchange) must reinsert each client in the other's position.
-    Thus, SWAP* should be able to identify additional improving moves beyond
-    those of the regular (1, 1)-exchange operator.
+    Thus, SWAP* should still be able to identify additional improving moves
+    after (1, 1)-exchange gets stuck.
     """
     data = read("data/RC208.txt", "solomon", "dimacs")
     pm = PenaltyManager(data.vehicle_capacity)
@@ -43,3 +44,27 @@ def test_swap_star_identifies_more_moves_than_regular_swap():
         # further improve the individual.
         assert_(swap_individual.cost() < individual.cost())
         assert_(swap_star_individual.cost() < swap_individual.cost())
+
+
+@mark.parametrize("seed", [2643, 2742, 2941, 3457, 4299, 4497, 6178, 6434])
+def test_RC208_instance(seed: int):
+    data = read("data/RC208.txt", "solomon", "dimacs")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=seed)
+
+    ls = LocalSearch(data, pm, rng, compute_neighbours(data))
+    ls.add_route_operator(SwapStar(data, pm))
+
+    # Make an initial solution that consists of two routes, by randomly
+    # splitting the single-route solution.
+    route = list(range(1, data.num_clients + 1))
+    split = rng.randint(data.num_clients)
+    individual = Individual(data, pm, [route[:split], route[split:]])
+    improved_individual = ls.intensify(
+        individual, overlap_tolerance_degrees=360
+    )
+
+    # The new solution should strictly improve on our original solution, but
+    # cannot use more routes since SWAP* does not create routes.
+    assert_equal(improved_individual.num_routes(), 2)
+    assert_(improved_individual.cost() < individual.cost())
