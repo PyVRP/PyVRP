de0390ce632bbd11a50b4a36dd94e8fadfdefa4a
Niels Wouda <nielswouda@gmail.com>
2023-02-27 11:07:09 +0100

    Add SWAP* test

--- pyvrp/educate/tests/test_SwapStar.py
+++ pyvrp/educate/tests/test_SwapStar.py
@@ -1,1 +1,41 @@
-# TODO
+from numpy.testing import assert_
+
+from pyvrp import Individual, PenaltyManager, XorShift128
+from pyvrp.educate import (
+    Exchange11,
+    LocalSearch,
+    NeighbourhoodParams,
+    SwapStar,
+    compute_neighbours,
+)
+from pyvrp.tests.helpers import read
+
+
+def test_swap_star_identifies_more_moves_than_regular_swap():
+    """
+    SWAP* can move two clients to any position in the routes, whereas regular
+    swap ((1, 1)-exchange) must reinsert each client in the other's position.
+    Thus, SWAP* should be able to identify more improving moves than the
+    regular (1, 1)-exchange move.
+    """
+    data = read("data/RC208.txt", "solomon", "dimacs")
+    pm = PenaltyManager(data.vehicle_capacity)
+    rng = XorShift128(seed=42)
+
+    # For a fair comparison we should not hamper the node operator with
+    # granularity restrictions.
+    nb_params = NeighbourhoodParams(nb_granular=data.num_clients)
+    ls = LocalSearch(data, pm, rng, compute_neighbours(data, nb_params))
+
+    ls.add_node_operator(Exchange11(data, pm))
+    ls.add_route_operator(SwapStar(data, pm))
+
+    for _ in range(10):  # repeat a few times to really make sure
+        individual = Individual(data, pm, rng)
+
+        swap_individual = ls.search(individual)
+        swap_star_individual = ls.intensify(
+            individual, overlap_tolerance_degrees=360
+        )
+
+        assert_(swap_star_individual.cost() < swap_individual.cost())
