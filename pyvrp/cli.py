7dd1dfe4842826f35a79216dc9e7c2f191651595
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-02-27 07:49:47 +0100

    New neighbourhood parameter values (#141)

--- pyvrp/cli.py
+++ pyvrp/cli.py
@@ -28,6 +28,7 @@ from pyvrp.educate import (
     NODE_OPERATORS,
     ROUTE_OPERATORS,
     LocalSearch,
+    NeighbourhoodParams,
     compute_neighbours,
 )
 from pyvrp.read import INSTANCE_FORMATS, ROUND_FUNCS, read
@@ -121,12 +122,14 @@ def solve(
 
     pen_params = PenaltyParams(**config.get("penalty", {}))
     pop_params = PopulationParams(**config.get("population", {}))
+    nb_params = NeighbourhoodParams(**config.get("neighbourhood", {}))
 
     data = read(data_loc, instance_format, round_func)
     rng = XorShift128(seed=seed)
     pen_manager = PenaltyManager(data.vehicle_capacity, pen_params)
     pop = Population(data, pen_manager, rng, bpd, pop_params)
-    ls = LocalSearch(data, pen_manager, rng, compute_neighbours(data))
+    neighbours = compute_neighbours(data, nb_params)
+    ls = LocalSearch(data, pen_manager, rng, neighbours)
 
     for op in NODE_OPERATORS:
         ls.add_node_operator(op(data, pen_manager))
