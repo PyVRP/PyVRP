806bab61c936b8f21336eb11d1b3d33d81eb3500
Niels Wouda <nielswouda@gmail.com>
2023-02-28 10:00:03 +0100

    Document SREX

--- pyvrp/crossover/_selective_route_exchange.pyi
+++ pyvrp/crossover/_selective_route_exchange.pyi
@@ -7,4 +7,34 @@ def selective_route_exchange(
     data: ProblemData,
     penalty_manager: PenaltyManager,
     rng: XorShift128,
-) -> Individual: ...
+) -> Individual:
+    """
+    This crossover operator due to Nagata and Kobayashi (2010) combines routes
+    from both parents to generate a new offspring solution. It does this by
+    carefully selecting routes from the second parent that could be exchanged
+    with routes from the first parent. After exchanging these routes, the
+    resulting offspring solution is repaired using a greedy repair strategy.
+
+    Parameters
+    ----------
+    parents
+        The two parent solutions to create an offspring from.
+    data
+        The problem instance.
+    penalty_manager
+        The penalty manager instance to be used during the greedy repair step.
+    rng
+        The random number generator to use.
+
+    Returns
+    -------
+    Individual
+        A new offspring.
+
+    References
+    ----------
+    .. [1] Nagata, Y., & Kobayashi, S. (2010). A Memetic Algorithm for the
+           Pickup and Delivery Problem with Time Windows Using Selective Route
+           Exchange Crossover. *Parallel Problem Solving from Nature*, PPSN XI,
+           536 - 545.
+    """
