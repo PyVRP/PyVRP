2925c2c5ee234b80c24ec9a9b386adbb1e42d180
Niels Wouda <nielswouda@gmail.com>
2023-03-02 13:43:43 +0100

    Further explain loadPenalty

--- pyvrp/cpp/PenaltyManager.h
+++ pyvrp/cpp/PenaltyManager.h
@@ -144,7 +144,9 @@ unsigned int PenaltyManager::loadPenaltyExcess(unsigned int excessLoad) const
 unsigned int PenaltyManager::loadPenalty(unsigned int load) const
 {
     // Branchless implementation: if load > capacity we return the excess load
-    // penalty; else zero. 
+    // penalty; else zero. Note that when load - vehicleCapacity wraps around,
+    // we return zero because load > vehicleCapacity evaluates as zero (so 
+    // there is not issue here due to unsignedness).
     return (load > vehicleCapacity) * loadPenaltyExcess(load - vehicleCapacity);
 }
 
