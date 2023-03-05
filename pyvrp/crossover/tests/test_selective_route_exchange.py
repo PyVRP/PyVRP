72f018f7d513685205276f876e5cbbd2c845c63a
l-lan <l.lan@vu.nl>
2023-03-05 21:58:09 +0100

    Rewrite tests

--- pyvrp/crossover/tests/test_selective_route_exchange.py
+++ pyvrp/crossover/tests/test_selective_route_exchange.py
@@ -2,6 +2,9 @@ from numpy.testing import assert_equal
 
 from pyvrp import Individual, PenaltyManager, XorShift128
 from pyvrp.crossover import selective_route_exchange as srex
+from pyvrp.crossover._selective_route_exchange import (
+    selective_route_exchange as _srex,
+)
 from pyvrp.tests.helpers import read
 
 
@@ -28,13 +31,12 @@ def test_srex_move_all_routes():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
-    # This RNG seed sets the internal SREX variable ``nMovedRoutes`` to 3.
-    rng = XorShift128(seed=4)
-
     indiv1 = Individual(data, pm, [[1], [2], [3, 4]])
     indiv2 = Individual(data, pm, [[1, 2], [3], [4]])
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 3)
+    # TODO add argument name
 
-    assert_equal(srex((indiv1, indiv2), data, pm, rng), indiv2)
+    assert_equal(offspring, indiv2)
 
 
 def test_srex_greedy_repair():
@@ -44,13 +46,11 @@ def test_srex_greedy_repair():
     data = read("data/OkSmallGreedyRepair.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=2)
-
-    indiv1 = Individual(data, pm, [[1, 2], [3, 4], []])
-    indiv2 = Individual(data, pm, [[2, 3], [4, 1], []])
-    parents = (indiv1, indiv2)
+    indiv1 = Individual(data, pm, [[1, 2], [3, 4]])
+    indiv2 = Individual(data, pm, [[2, 3], [4, 1]])
 
     # The start indices do not change because there are no improving moves.
     # So, indiv1's route [1, 2] will be replaced by indiv2's route [2, 3].
@@ -58,7 +58,7 @@ def test_srex_greedy_repair():
     # which are both repaired using greedy repair. After repair, we obtain the
     # offspring [[2, 3, 1], [4]] with cost 8735, and [[1, 2], [3, 4]] with
     # cost 9725. The first one is returned since it has the lowest cost.
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[2, 3, 1], [4], []])
 
@@ -70,13 +70,11 @@ def test_srex_changed_start_indices():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=2)
-
-    indiv1 = Individual(data, pm, [[1, 2, 3], [4], []])
-    indiv2 = Individual(data, pm, [[1, 2, 4], [3], []])
-    parents = (indiv1, indiv2)
+    indiv1 = Individual(data, pm, [[1, 2, 3], [4]])
+    indiv2 = Individual(data, pm, [[1, 2, 4], [3]])
 
     # The start indices at initialization are startA = 0 and startB = 0.
     # The difference for A to move left (= right) is -1. The difference for B
@@ -87,7 +85,7 @@ def test_srex_changed_start_indices():
     # This results in two candidate offspring, [[3], [1, 2, 4]] with cost
     # 10195, and [[1, 2, 3], [4]] with cost 31029. The first candidate is
     # returned since it has the lowest cost.
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[3], [1, 2, 4], []])
 
@@ -100,13 +98,12 @@ def test_srex_a_left_move():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=17)
 
     indiv1 = Individual(data, pm, [[1, 3], [2], [4]])
     indiv2 = Individual(data, pm, [[4, 1], [2], [3]])
-    parents = (indiv1, indiv2)
 
     # We describe the A-left case here in detail. The tests below for A-right,
     # B-left and B-right can be worked out similarly: note that we only change
@@ -137,7 +134,7 @@ def test_srex_a_left_move():
     # Candidate offspring
     # [1, 3] [2] [4] - cost: 24416
     # [3] [2] [4, 1] - cost: 12699 <-- selected as new offspring
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[3], [2], [4, 1]])
 
@@ -150,15 +147,12 @@ def test_srex_a_right_move():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=17)
-
     indiv1 = Individual(data, pm, [[1, 3], [4], [2]])
     indiv2 = Individual(data, pm, [[4, 1], [2], [3]])
-    parents = (indiv1, indiv2)
-
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[3], [4, 1], [2]])
 
@@ -171,15 +165,12 @@ def test_srex_b_left_move():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=17)
-
     indiv1 = Individual(data, pm, [[4], [2], [1, 3]])
     indiv2 = Individual(data, pm, [[3], [2], [4, 1]])
-    parents = (indiv1, indiv2)
-
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[4, 1], [2], [3]])
 
@@ -192,14 +183,12 @@ def test_srex_b_right_move():
     data = read("data/OkSmall.txt")
     pm = PenaltyManager(data.vehicle_capacity)
 
+    # TODO remove
     # This RNG seed sets the internal SREX variables ``startA = 0`` and
     # ``nMovedRoutes = 1``.
-    rng = XorShift128(seed=17)
-
     indiv1 = Individual(data, pm, [[4], [2], [1, 3]])
     indiv2 = Individual(data, pm, [[3], [4, 1], [2]])
-    parents = (indiv1, indiv2)
 
-    offspring = srex(parents, data, pm, rng)
+    offspring = _srex((indiv1, indiv2), data, pm, 0, 0, 1)
 
     assert_equal(offspring.get_routes(), [[4, 1], [2], [3]])
