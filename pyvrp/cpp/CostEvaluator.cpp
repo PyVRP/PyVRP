d03d82d8c018efeb05c5566cf8cc652698001ea9
Wouter Kool <1150455+wouterkool@users.noreply.github.com>
2023-03-21 13:44:56 +0100

    Remove unused import

--- pyvrp/cpp/CostEvaluator.cpp
+++ pyvrp/cpp/CostEvaluator.cpp
@@ -1,7 +1,5 @@
 #include "CostEvaluator.h"
 
-#include <numeric>
-
 CostEvaluator::CostEvaluator(unsigned int capacityPenalty,
                              unsigned int timeWarpPenalty)
     : capacityPenalty(capacityPenalty), timeWarpPenalty(timeWarpPenalty)
