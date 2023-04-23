fad5a2ae45b78f0a9badaa531da9c96edcf625df
Niels Wouda <nielswouda@gmail.com>
2023-04-23 18:15:54 +0200

    Basic typing and conversion support, and Python bindings

--- pyvrp/cpp/Measure.h
+++ pyvrp/cpp/Measure.h
@@ -19,28 +19,52 @@ template <MeasureType _> class Measure
     friend class Measure<MeasureType::DURATION>;
     friend class Measure<MeasureType::COST>;
 
-    value_type const value;
+    value_type value;
 
 public:
-    Measure(value_type const value) : value(value){};
+    // Default construction initialises to 0. The other constructors take both
+    // precisions (generally useful), but do case that value to the underlying
+    // value type.
+    Measure() = default;
+    Measure(int const value) : value(static_cast<value_type>(value)){};
+    Measure(double const value) : value(static_cast<value_type>(value)){};
+
+    // Explicit conversion of the underlying value to int and double.
+    explicit operator int() const { return static_cast<int>(value); }
+    explicit operator double() const { return static_cast<double>(value); }
 
     // Explicit conversions to other measures.
     explicit operator Measure<MeasureType::DISTANCE>() const { return {value}; }
     explicit operator Measure<MeasureType::DURATION>() const { return {value}; }
     explicit operator Measure<MeasureType::COST>() const { return {value}; }
 
-    // Explicit conversion to the underlying value.
-    explicit operator value_type() const { return value; }
-
     // Arithmetic operators.
-    Measure operator+(Measure const &other) { return {value + other.value}; }
-    Measure operator-(Measure const &other) { return {value - other.value}; }
-    Measure operator*(Measure const &other) { return {value * other.value}; }
-    Measure operator/(Measure const &other) { return {value / other.value}; }
+    Measure operator+(Measure const &other) const
+    {
+        return {value + other.value};
+    }
+
+    Measure operator-(Measure const &other) const
+    {
+        return {value - other.value};
+    }
+
+    Measure operator*(Measure const &other) const
+    {
+        return {value * other.value};
+    }
+
+    Measure operator/(Measure const &other) const
+    {
+        return {value / other.value};
+    }
 
     // Comparison operators.
-    auto operator==(Measure const &other) { return value == other.value; }
-    auto operator<=>(Measure const &other) { return value <=> other.value; }
+    auto operator==(Measure const &other) const { return value == other.value; }
+    auto operator<=>(Measure const &other) const
+    {
+        return value <=> other.value;
+    }
 };
 
 using distance_type = Measure<MeasureType::DISTANCE>;
--- pyvrp/cpp/Measure_bindings.cpp
+++ pyvrp/cpp/Measure_bindings.cpp
@@ -7,7 +7,34 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(_Measure, m)
 {
-    py::class_<Measure<MeasureType::DISTANCE>>(m, "distance_type");
-    py::class_<Measure<MeasureType::DURATION>>(m, "duration_type");
-    py::class_<Measure<MeasureType::COST>>(m, "cost_type");
+#ifdef INT_PRECISION
+    m.attr("has_integer_precision") = false;
+#else
+    m.attr("has_integer_precision") = true;
+#endif
+
+    py::class_<distance_type>(m, "distance_type")
+        .def(py::init<int>())
+        .def(py::init<double>())
+        .def("__int__",
+             [](distance_type const dist) { return static_cast<int>(dist); })
+        .def("__float__", [](distance_type const dist) {
+            return static_cast<double>(dist);
+        });
+
+    py::class_<duration_type>(m, "duration_type")
+        .def(py::init<int>())
+        .def(py::init<double>())
+        .def("__int__",
+             [](duration_type const dur) { return static_cast<int>(dur); })
+        .def("__float__",
+             [](duration_type const dur) { return static_cast<double>(dur); });
+
+    py::class_<cost_type>(m, "cost_type")
+        .def(py::init<int>())
+        .def(py::init<double>())
+        .def("__int__",
+             [](cost_type const cost) { return static_cast<int>(cost); })
+        .def("__float__",
+             [](cost_type const cost) { return static_cast<double>(cost); });
 }
