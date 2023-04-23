d8a54caf5a31f1312a4fbb2f8fb05f6c2ef5f173
Niels Wouda <nielswouda@gmail.com>
2023-04-23 23:13:38 +0200

    Generalise the Measure implementation a bit

--- pyvrp/cpp/Measure.h
+++ pyvrp/cpp/Measure.h
@@ -1,4 +1,8 @@
+#ifndef MEASURE_H
+#define MEASURE_H
+
 #include <compare>
+#include <type_traits>
 
 #ifdef INT_PRECISION
 using value_type = int;
@@ -22,42 +26,41 @@ template <MeasureType _> class Measure
     value_type value;
 
 public:
-    // Default construction initialises to 0. The other constructors take both
-    // precisions (generally useful), but do case that value to the underlying
-    // value type.
+    // Default construction initialises to 0.
     Measure() = default;
-    Measure(int const value) : value(static_cast<value_type>(value)){};
-    Measure(double const value) : value(static_cast<value_type>(value)){};
 
-    // Explicit conversion of the underlying value to int and double.
-    explicit operator int() const { return static_cast<int>(value); }
-    explicit operator double() const { return static_cast<double>(value); }
+    // This constructor takes any arithmetic type (generally useful) and casts
+    // its value to value_type.
+    template <typename T,
+              std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
+    Measure(T const value = 0) : value(static_cast<value_type>(value))
+    {
+    }
+
+    // Explicit conversions of the underlying value to other arithmetic types.
+    template <typename T,
+              std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
+    explicit operator T() const
+    {
+        return static_cast<T>(value);
+    }
+
+    Measure &operator+=(Measure const &rhs)
+    {
+        this->value += rhs.value;
+        return *this;
+    }
+
+    Measure &operator-=(Measure const &rhs)
+    {
+        this->value -= rhs.value;
+        return *this;
+    }
 
     // Explicit conversions to other measures.
     explicit operator Measure<MeasureType::DISTANCE>() const { return {value}; }
     explicit operator Measure<MeasureType::DURATION>() const { return {value}; }
     explicit operator Measure<MeasureType::COST>() const { return {value}; }
-
-    // Arithmetic operators.
-    Measure operator+(Measure const &other) const
-    {
-        return {value + other.value};
-    }
-
-    Measure operator-(Measure const &other) const
-    {
-        return {value - other.value};
-    }
-
-    Measure operator*(Measure const &other) const
-    {
-        return {value * other.value};
-    }
-
-    Measure operator/(Measure const &other) const
-    {
-        return {value / other.value};
-    }
 
     // Comparison operators.
     auto operator==(Measure const &other) const { return value == other.value; }
@@ -67,6 +70,33 @@ public:
     }
 };
 
+template <MeasureType Type>
+Measure<Type> operator+(Measure<Type> const lhs, Measure<Type> const rhs)
+{
+    return static_cast<value_type>(lhs) + static_cast<value_type>(rhs);
+}
+
+template <MeasureType Type>
+Measure<Type> operator-(Measure<Type> const lhs, Measure<Type> const rhs)
+{
+    return static_cast<value_type>(lhs) - static_cast<value_type>(rhs);
+}
+
+template <MeasureType Type>
+Measure<Type> operator*(Measure<Type> const lhs, Measure<Type> const rhs)
+{
+    return static_cast<value_type>(lhs) * static_cast<value_type>(rhs);
+}
+
+template <MeasureType Type>
+Measure<Type> operator/(Measure<Type> const lhs, Measure<Type> const rhs)
+{
+    return static_cast<value_type>(lhs) / static_cast<value_type>(rhs);
+}
+
+// Useful type aliases.
 using distance_type = Measure<MeasureType::DISTANCE>;
 using duration_type = Measure<MeasureType::DURATION>;
 using cost_type = Measure<MeasureType::COST>;
+
+#endif  // MEASURE_H
