0f3c7a1f689e878a6015c5e98e7a1f6d150e72a5
Wouter Kool <1150455+wouterkool@users.noreply.github.com>
2023-04-26 22:12:24 +0200

    Add checks to Client constructor

--- pyvrp/cpp/ProblemData.h
+++ pyvrp/cpp/ProblemData.h
@@ -18,6 +18,27 @@ public:
         int serviceDuration;  // Service duration
         int twEarly;          // Earliest arrival (when using time windows)
         int twLate;           // Latest arrival (when using time windows)
+
+        Client(int x,
+               int y,
+               int demand,
+               int serviceDuration,
+               int twEarly,
+               int twLate)
+            : x(x),
+              y(y),
+              demand(demand),
+              serviceDuration(serviceDuration),
+              twEarly(twEarly),
+              twLate(twLate)
+        {
+            if (demand < 0)
+                throw std::invalid_argument("demand must be >= 0");
+            if (serviceDuration < 0)
+                throw std::invalid_argument("service_duration must be >= 0");
+            if (twEarly > twLate)
+                throw std::invalid_argument("tw_early must be <= tw_late");
+        }
     };
 
 private:
--- pyvrp/tests/test_ProblemData.py
+++ pyvrp/tests/test_ProblemData.py
@@ -1,6 +1,54 @@
-from numpy.testing import assert_
+from numpy.testing import assert_, assert_equal, assert_raises
+from pytest import mark
 
 from pyvrp import Client, ProblemData
+
+
+@mark.parametrize(
+    "x,y,demand,service_duration,tw_early,tw_late",
+    [
+        (1, 1, 1, 1, 0, 1),  # normal
+        (1, 1, 1, 0, 0, 1),  # zero duration
+        (1, 1, 0, 1, 0, 1),  # zero demand
+        (1, 1, 1, 1, 0, 0),  # zero length time interval
+        (-1, -1, 1, 1, 0, 1),  # negative coordinates
+    ],
+)
+def test_valid_client(
+    x: int,
+    y: int,
+    demand: int,
+    service_duration: int,
+    tw_early: int,
+    tw_late: int,
+):
+    client = Client(x, y, demand, service_duration, tw_early, tw_late)
+    assert_equal(client.x, x)
+    assert_equal(client.y, y)
+    assert_equal(client.demand, demand)
+    assert_equal(client.service_duration, service_duration)
+    assert_equal(client.tw_early, tw_early)
+    assert_equal(client.tw_late, tw_late)
+
+
+@mark.parametrize(
+    "x,y,demand,service_duration,tw_early,tw_late",
+    [
+        (1, 1, 1, 1, 1, 0),  # late < early
+        (1, 1, 1, -1, 0, 1),  # negative duration
+        (1, 1, -1, 1, 0, 1),  # negative demand
+    ],
+)
+def test_invalid_client(
+    x: int,
+    y: int,
+    demand: int,
+    service_duration: int,
+    tw_early: int,
+    tw_late: int,
+):
+    with assert_raises(ValueError):
+        Client(x, y, demand, service_duration, tw_early, tw_late)
 
 
 def test_depot_is_first_client():
