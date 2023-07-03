96fcb08af6c908a2a573612a6a6e851033bc82db
Niels Wouda <nielswouda@gmail.com>
2023-07-03 16:31:08 +0200

    Split decl/impl files, add to build

---
+++ pyvrp/cpp/DynamicBitset.cpp
@@ -0,0 +1,82 @@
+#include "DynamicBitset.h"
+
+DynamicBitset::DynamicBitset(size_t numBits)
+{
+    auto const q = numBits / BLOCK_SIZE;
+    auto const r = numBits % BLOCK_SIZE;
+    data_ = {q + r > 0, 0};  // + 1 if the division is not perfect
+};
+
+DynamicBitset::DynamicBitset(std::vector<Block> data)
+    : data_(std::move(data)){};
+
+bool DynamicBitset::operator==(DynamicBitset const &other) const
+{
+    return data_ == other.data_;
+}
+
+DynamicBitset &DynamicBitset::operator&=(DynamicBitset const &other)
+{
+    assert(size() == other.size());  // assumed true during runtime
+
+    for (size_t idx = 0; idx != data_.size(); ++idx)
+        data_[idx] &= other.data_[idx];
+
+    return *this;
+}
+
+size_t DynamicBitset::count() const
+{
+    size_t count = 0;
+    for (auto const &bitset : data_)
+        count += bitset.count();
+    return count;
+}
+
+size_t DynamicBitset::size() const { return 64 * data_.size(); }
+
+DynamicBitset &DynamicBitset::operator|=(DynamicBitset const &other)
+{
+    assert(size() == other.size());  // assumed true during runtime
+
+    for (size_t idx = 0; idx != data_.size(); ++idx)
+        data_[idx] |= other.data_[idx];
+
+    return *this;
+}
+
+DynamicBitset &DynamicBitset::operator^=(DynamicBitset const &other)
+{
+    assert(size() == other.size());  // assumed true during runtime
+
+    for (size_t idx = 0; idx != data_.size(); ++idx)
+        data_[idx] ^= other.data_[idx];
+
+    return *this;
+}
+
+DynamicBitset DynamicBitset::operator~() const
+{
+    std::vector<Block> copy(data_);
+    for (size_t idx = 0; idx != copy.size(); ++idx)
+        copy[idx] = ~copy[idx];
+    return copy;
+}
+
+DynamicBitset DynamicBitset::operator|(DynamicBitset const &other) const
+{
+    std::vector<Block> copy(data_);
+    return DynamicBitset(copy) |= other;
+}
+
+DynamicBitset DynamicBitset::operator&(DynamicBitset const &other) const
+{
+    std::vector<Block> copy(data_);
+    return DynamicBitset(copy) &= other;
+}
+
+DynamicBitset DynamicBitset::operator^(DynamicBitset const &other) const
+{
+    std::vector<Block> copy(data_);
+    return DynamicBitset(copy) ^= other;
+}
--- pyvrp/cpp/DynamicBitset.h
+++ pyvrp/cpp/DynamicBitset.h
@@ -6,7 +6,8 @@
 #include <vector>
 
 /**
- * A simple dynamic bitset implementation, on top of a vector of bitsets.
+ * A simple dynamic bitset implementation, on top of a vector of bitsets. See
+ * https://en.cppreference.com/w/cpp/utility/bitset for details.
  */
 class DynamicBitset
 {
@@ -16,99 +17,40 @@ class DynamicBitset
     std::vector<Block> data_;
 
 public:
-    DynamicBitset(size_t numBits)
-    {
-        auto const q = numBits / BLOCK_SIZE;
-        auto const r = numBits % BLOCK_SIZE;
-        data_ = {q + r > 0, 0};
-    };
+    DynamicBitset(size_t numBits);
+    DynamicBitset(std::vector<Block> data);
 
-    DynamicBitset(std::vector<Block> data) : data_(std::move(data)){};
+    [[nodiscard]] bool operator==(DynamicBitset const &other) const;
 
-    [[nodiscard]] inline bool operator==(DynamicBitset const &other) const
-    {
-        return data_ == other.data_;
-    }
+    [[nodiscard]] inline bool operator[](size_t idx) const;
+    [[nodiscard]] inline Block::reference operator[](size_t idx);
 
-    [[nodiscard]] inline bool operator[](size_t idx) const
-    {
-        auto const q = idx / BLOCK_SIZE;
-        auto const r = idx % BLOCK_SIZE;
-        return data_[q][r];
-    }
+    [[nodiscard]] size_t count() const;
 
-    [[nodiscard]] inline Block::reference operator[](size_t idx)
-    {
-        auto const q = idx / BLOCK_SIZE;
-        auto const r = idx % BLOCK_SIZE;
-        return data_[q][r];
-    }
+    [[nodiscard]] size_t size() const;
 
-    [[nodiscard]] inline size_t count() const
-    {
-        size_t count = 0;
-        for (auto const &bitset : data_)
-            count += bitset.count();
-        return count;
-    }
+    DynamicBitset &operator&=(DynamicBitset const &other);
+    DynamicBitset &operator|=(DynamicBitset const &other);
+    DynamicBitset &operator^=(DynamicBitset const &other);
 
-    [[nodiscard]] inline size_t size() const { return 64 * data_.size(); }
-
-    DynamicBitset &operator&=(DynamicBitset const &other)
-    {
-        assert(size() == other.size());  // assumed true during runtime
-
-        for (size_t idx = 0; idx != data_.size(); ++idx)
-            data_[idx] &= other.data_[idx];
-
-        return *this;
-    }
-
-    DynamicBitset &operator|=(DynamicBitset const &other)
-    {
-        assert(size() == other.size());  // assumed true during runtime
-
-        for (size_t idx = 0; idx != data_.size(); ++idx)
-            data_[idx] |= other.data_[idx];
-
-        return *this;
-    }
-
-    DynamicBitset &operator^=(DynamicBitset const &other)
-    {
-        assert(size() == other.size());  // assumed true during runtime
-
-        for (size_t idx = 0; idx != data_.size(); ++idx)
-            data_[idx] ^= other.data_[idx];
-
-        return *this;
-    }
-
-    DynamicBitset operator~() const
-    {
-        std::vector<Block> copy(data_);
-        for (size_t idx = 0; idx != copy.size(); ++idx)
-            copy[idx] = ~copy[idx];
-        return copy;
-    }
-
-    [[nodiscard]] DynamicBitset operator|(DynamicBitset const &other) const
-    {
-        std::vector<Block> copy(data_);
-        return DynamicBitset(copy) |= other;
-    }
-
-    [[nodiscard]] DynamicBitset operator&(DynamicBitset const &other) const
-    {
-        std::vector<Block> copy(data_);
-        return DynamicBitset(copy) &= other;
-    }
-
-    [[nodiscard]] DynamicBitset operator^(DynamicBitset const &other) const
-    {
-        std::vector<Block> copy(data_);
-        return DynamicBitset(copy) ^= other;
-    }
+    [[nodiscard]] DynamicBitset operator|(DynamicBitset const &other) const;
+    [[nodiscard]] DynamicBitset operator&(DynamicBitset const &other) const;
+    [[nodiscard]] DynamicBitset operator^(DynamicBitset const &other) const;
+    [[nodiscard]] DynamicBitset operator~() const;
 };
+
+bool DynamicBitset::operator[](size_t idx) const
+{
+    auto const q = idx / BLOCK_SIZE;
+    auto const r = idx % BLOCK_SIZE;
+    return data_[q][r];
+}
+
+DynamicBitset::Block::reference DynamicBitset::operator[](size_t idx)
+{
+    auto const q = idx / BLOCK_SIZE;
+    auto const r = idx % BLOCK_SIZE;
+    return data_[q][r];
+}
 
 #endif  // PYVRP_DYNAMICBITSET_H
