04fc60a3c37ea9f10faf7d3eac77487562bdd27d
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-07-03 09:58:19 +0200

    Update pyvrp/_Solution.pyi

--- pyvrp/_Solution.pyi
+++ pyvrp/_Solution.pyi
@@ -51,7 +51,7 @@ class Route:
         """
     def release_time(self) -> int:
         """
-        Release time (that is, earliest time to leave depot) of this route.
+        Release time (that is, the earliest time allowed to leave the depot) of this route.
         """
     def prizes(self) -> int:
         """
