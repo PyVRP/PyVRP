275cd011a656c3401299c36b48fb45bbb12da310
Leon Lan <l.lan@vu.nl>
2023-07-04 12:15:27 +0200

    Changes to split_giant_tour

--- pyvrp/crossover/split_giant_tour.py
+++ pyvrp/crossover/split_giant_tour.py
@@ -11,48 +11,67 @@ class Route:
     load: int
 
 
-def split_giant_tour(tour: List[int], data: ProblemData) -> List[List[int]]:
+def split_giant_tour(
+    tour: List[int], vehicle_types: List[int], data: ProblemData
+) -> List[List[int]]:
     """
-    Splits a giant tour into a list of routes in a naive way. The giant tour
-    sequentially visits all clients until it is no longer feasible to add a
-    client to the current route. Then, a new route is started. This is repeated
-    until all clients have been visited.
-    """
-    depot = 0
-    prev_client = depot
-    route = Route([], data.client(depot).tw_early, 0)
-    routes = [route]
+    Splits a giant tour into a list of routes. We start with an empty route and
+    sequentially add clients until it is no longer feasible for the given
+    route type. We repeat this until all clients are visited, or until all
+    vehicle are used.
 
-    for idx, client in enumerate(tour):
-        if len(routes) == data.num_vehicles:
-            # If there is only one vehicle left, we have to add all remaining
-            # clients to the last route.
+    Parameters
+    ----------
+    tour
+        A giant tour of client visits.
+    vehicle_types
+        The vehicle types to use for the routes.
+    data
+        The problem data.
+    """
+    routes = []
+    depot = 0
+    tour_idx = 0
+
+    for idx, vehicle_type_idx in enumerate(vehicle_types):
+        vehicle_type = data.vehicle_type(vehicle_type_idx)
+
+        route = Route([], data.client(depot).tw_early, 0)
+        prev = depot
+
+        if idx == len(vehicle_types) - 1:
+            # If there is only one vehicle left, we add all remaining clients
+            # to the last route.
             route.clients.extend(tour[idx:])
             break
 
-        client_arrive = route.time + data.duration(prev_client, client)
-        client_start = max(client_arrive, data.client(client).tw_early)
-        client_finish = client_start + data.client(client).service_duration
-        depot_arrive = client_finish + data.duration(client, depot)
+        while tour_idx < len(tour):
+            client = tour[tour_idx]
 
-        if (
-            route.load + data.client(client).demand
-            > data.vehicle_type(0).capacity
-            or client_arrive > data.client(client).tw_late
-            or depot_arrive > data.client(depot).tw_late
-        ):
-            # Adding the client is not feasible, so we start a new route.
-            prev_client = depot
-            early = data.client(depot).tw_early + data.duration(depot, client)
-            route_start = max(data.client(client).tw_early, early)
-            route_load = data.client(client).demand
+            arrive_at_client = route.time + data.duration(prev, client)
+            start_at_client = max(
+                arrive_at_client, data.client(client).tw_early
+            )
+            finish_at_client = (
+                start_at_client + data.client(client).service_duration
+            )
+            return_to_depot = finish_at_client + data.duration(client, depot)
 
-            route = Route([client], route_start, route_load)
-            routes.append(route)
-        else:
-            prev_client = client
-            route.clients.append(client)
-            route.time = client_finish
-            route.load += data.client(client).demand
+            if (
+                route.load + data.client(client).demand > vehicle_type.capacity
+                or arrive_at_client > data.client(client).tw_late
+                or return_to_depot > data.client(depot).tw_late
+            ):
+                # Adding the current client to the route makes it infeasible,
+                # so we stop here and use the next vehicle type.
+                routes.append(route)
+                break
+            else:
+                prev = client
+                route.clients.append(client)
+                route.time = finish_at_client
+                route.load += data.client(client).demand
+
+                tour_idx += 1
 
     return [route.clients for route in routes if route.clients]
--- pyvrp/crossover/tests/test_split_giant_tour.py
+++ pyvrp/crossover/tests/test_split_giant_tour.py
@@ -5,35 +5,48 @@ from pyvrp.tests.helpers import read
 
 
 def test_split_giant_tour():
-    data = read("data/OkSmallSplitTour.txt")
+    # TODO find better name
+    data = read("data/OkSmall.txt")
 
     # The tour is ordered in increasing order of earliest time window and
-    # created such a route is feasible.
+    # created, so a single route is feasible.
     tour = [3, 2, 1, 4]
-    routes = split_giant_tour(tour, data)
+    vehicle_types = [0, 0, 0]
+    routes = split_giant_tour(tour, vehicle_types, data)
+
     assert_equal(len(routes), 1)
     assert_equal(routes, [[3, 2, 1, 4]])
 
     # The earliest time window of client 4 is later than the other time
     # windows, so we need to split the tour into two routes.
     tour = [4, 3, 2, 1]
-    routes = split_giant_tour(tour, data)
+    vehicle_types = [0, 0, 0]
+    routes = split_giant_tour(tour, vehicle_types, data)
 
     assert_equal(len(routes), 2)
     assert_equal(routes, [[4], [3, 2, 1]])
 
 
-def test_split_giant_tour_ignores_feasibility_last_vehicle():
+def test_put_all_clients_to_last_vehicle():
+    # TODO Single route
+
+    # TODO two routes, but three needed
+    pass
+
+
+def test_ignore_feasibility_last_vehicle():
     """
-    Tests that all remaining clients are added to the last vehicle if the
-    number of routes is equal to the number of vehicles, regardless of
+    Tests that all remaining clients are added to the last vehicle, regardless
     whether the resulting route is feasible or not.
     """
-    data = read("data/OkSmallSplitTour.txt")
+    data = read("data/OkSmall.txt")
     tour = [4, 1, 3, 2]
-    routes = split_giant_tour(tour, data)
 
-    assert_equal(len(routes), 3)
+    # We only have one vehicle type, so we can only create one route.
+    vehicle_types = [0]
+    routes = split_giant_tour(tour, vehicle_types, data)
+    assert_equal(len(routes), 1)
+    assert_equal(routes, [[4, 1, 3, 2]])
 
     # 3 -> 2 is not feasible, but the maximum number of routes is 3.
     assert_equal(routes, [[4], [1], [3, 2]])
