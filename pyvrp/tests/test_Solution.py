9616df3de508bb87b360c465957595246fd35467
Leon Lan <l.lan@vu.nl>
2023-07-04 09:52:33 +0200

    Compute incurred time warp of infeasible solution release times

--- pyvrp/tests/test_Solution.py
+++ pyvrp/tests/test_Solution.py
@@ -179,11 +179,19 @@ def test_feasibility():
 def test_feasibility_release_times():
     data = read("data/OkSmallReleaseTimes.txt")
 
-    # Client 1 is released at 20000, but client 2 time window ends at 19500.
+    # Client 1 is released at 20'000, but client 2 time window ends at 19'500,
+    # so this solution must be infeasible due to time-warping. We arrive at
+    # client 1 at time 20'000 + 1'544 = 21'544 before the TW closes (22'500).
+    # We arrive at client 2 at 21'544 + 360 + 1'992 = 23'896, so we incur a
+    # time warp of 23'896 - 19'500 = 4'396.
     sol = Solution(data, [[1, 2], [3], [4]])
     assert_(not sol.is_feasible())
+    assert_equal(sol.time_warp(), 4396)
 
-    # Visiting clients 2 and 3 together is feasible.
+    # Visiting clients 2 and 3 together is feasible: both clients are released
+    # at time 5'000. We arrive at client 2 at 5'000 + 1'944 and wait till the
+    # TW opens (12'000). We arrive at client 3 at 12'000 + 360 + 621 = 12'981,
+    # which is before the TW closes (15'300).
     sol = Solution(data, [[1], [2, 3], [4]])
     assert_(sol.is_feasible())
 
