9773a113cd4036ca674c02cbc46bce472777bafb
Leon Lan <l.lan@vu.nl>
2023-07-04 10:45:43 +0200

    Make giant tour

--- pyvrp/crossover/make_giant_tour.py
+++ pyvrp/crossover/make_giant_tour.py
@@ -5,9 +5,17 @@ from pyvrp._Solution import Route
 
 def make_giant_tour(routes: List[Route]) -> List[int]:
     """
-    Makes a giant tour of the given routes. The giant tour representation
-    is a list of clients, where each client is represented by its index in the
-    problem data.
+    Makes a giant tour of the passed-in routes.
+
+    Parameters
+    ----------
+    routes
+        The routes to be merged into a giant tour.
+
+    Returns
+    -------
+    List[int]
+        The giant tour representation of the given routes.
     """
     sorted_routes = sorted(routes, key=bool)
     return [client for route in sorted_routes for client in route]
