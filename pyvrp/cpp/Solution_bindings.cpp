44cf96d0f5706fbb26794e9fb42a3316cb8af4f8
Niels Wouda <N-Wouda@users.noreply.github.com>
2023-07-07 17:56:19 +0200

    Add dynamic bitset (#256)
    * Remove unused includes in SREX
    
    * Implement our own dynamic bitset, simplify
    
    * Use DynamicBitset for crossover::greedyRepair
    
    * Split decl/impl files, add to build
    
    * Make gcc happy
    
    * Add a few basic tests
    
    * Test binary operators
    
    * Test operator==
    
    * Move include to source file
    
    * Add version to Meson build, remove redundant parentheses in binary op
    
    * Properly type argument in __eq__

--- pyvrp/cpp/Solution_bindings.cpp
+++ pyvrp/cpp/Solution_bindings.cpp
@@ -69,7 +69,7 @@ PYBIND11_MODULE(_Solution, m)
                 return route[idx];
             },
             py::arg("idx"))
-        .def(py::self == py::self)  // this is __eq__
+        .def(py::self == py::self, py::arg("other"))  // this is __eq__
         .def("__str__", [](Solution::Route const &route) {
             std::stringstream stream;
             stream << route;
@@ -129,7 +129,7 @@ PYBIND11_MODULE(_Solution, m)
             py::arg("memo"))
         .def("__hash__",
              [](Solution const &sol) { return std::hash<Solution>()(sol); })
-        .def(py::self == py::self)  // this is __eq__
+        .def(py::self == py::self, py::arg("other"))  // this is __eq__
         .def("__str__", [](Solution const &sol) {
             std::stringstream stream;
             stream << sol;
