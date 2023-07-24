4bd3190742eaa4a8c0a323be80172db4c6d99077
Niels Wouda <nielswouda@gmail.com>
2023-07-24 18:57:14 +0200

    Update signature

--- pyvrp/search/SearchMethod.py
+++ pyvrp/search/SearchMethod.py
@@ -1,6 +1,6 @@
 from typing import Protocol
 
-from pyvrp import Solution
+from pyvrp import CostEvaluator, Solution
 
 
 class SearchMethod(Protocol):  # pragma: no cover
@@ -8,7 +8,11 @@ class SearchMethod(Protocol):  # pragma: no cover
     Protocol that search methods must implement.
     """
 
-    def __call__(self, solution: Solution) -> Solution:
+    def __call__(
+        self,
+        solution: Solution,
+        cost_evaluator: CostEvaluator,
+    ) -> Solution:
         """
         Search around the given solution, and returns a new solution that is
         hopefully better.
@@ -17,10 +21,11 @@ class SearchMethod(Protocol):  # pragma: no cover
         ----------
         solution
             The solution to improve.
+        cost_evaluator
+            Cost evaluator to use when evaluating improvements.
 
         Returns
         -------
         Solution
             The improved solution.
         """
-        ...
