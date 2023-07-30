95f3dccc2d293b6b13f0dabf9fc5cbc7fb3833ee
Leon Lan <l.lan@vu.nl>
2023-07-30 14:32:07 -0700

    Add TODO

--- pyvrp/search/tests/test_MoveTwoClientsReversed.py
+++ pyvrp/search/tests/test_MoveTwoClientsReversed.py
@@ -1,4 +1,4 @@
-from numpy.testing import assert_, assert_equal
+from numpy.testing import assert_
 from pytest import mark
 
 from pyvrp import CostEvaluator, RandomNumberGenerator, Solution
@@ -11,40 +11,19 @@ from pyvrp.search import (
 from pyvrp.tests.helpers import read
 
 
-def test_single_route_OkSmall():
+@mark.parametrize("seed", list(range(50)))
+def test_single_route_OkSmall(seed):
     """
     This test checks that MoveTwoClientsReversed properly solves the small
     instance where we know what is going on.
     """
-    data = read("data/OkSmall.txt")
-    cost_evaluator = CostEvaluator(20, 6)
-    rng = RandomNumberGenerator(seed=42)
+    read("data/OkSmall.txt")
+    RandomNumberGenerator(seed)
 
-    nb_params = NeighbourhoodParams(nb_granular=data.num_clients)
-    ls = LocalSearch(data, rng, compute_neighbours(data, nb_params))
-    ls.add_node_operator(MoveTwoClientsReversed(data))
+    # No capacity penalty so we can visit all clients using a single route.
+    CostEvaluator(0, 10000)
 
-    sol = Solution(data, [[1, 3], [2, 4]])
-    improved_sol = ls.search(sol, cost_evaluator)
-
-    # The new solution should strictly improve on our original solution.
-    assert_equal(improved_sol.num_routes(), 1)
-    current_cost = cost_evaluator.penalised_cost(sol)
-    improved_cost = cost_evaluator.penalised_cost(improved_sol)
-    assert_(improved_cost < current_cost)
-
-    # (1, 3) was inserted after 4 as 2 -> 4 -> 3 -> 1.
-    expected = Solution(data, [[2, 4, 3, 1]])
-    assert_equal(improved_sol, expected)
-
-    # These two-route solutions can all be created by MoveTwoClientsReversed
-    # from the returned solution. So they must have a cost that's at best equal
-    # to the returned solution's cost.
-    for routes in ([[3, 1], [4, 2]], [[2, 1], [3, 4]], [[2, 4], [1, 3]]):
-        other = Solution(data, routes)
-        improved_cost = cost_evaluator.penalised_cost(improved_sol)
-        other_cost = cost_evaluator.penalised_cost(other)
-        assert_(improved_cost <= other_cost)
+    # TODO rewrite this test
 
 
 @mark.parametrize("seed", [2643, 2742, 2941, 3457, 4299, 4497, 6178, 6434])
