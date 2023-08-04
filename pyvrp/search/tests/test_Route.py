47f40ad3810fb200ef98feed11b9d9b33dab1284
Niels Wouda <nielswouda@gmail.com>
2023-08-04 12:51:03 +0200

    Add a basic test

--- pyvrp/search/tests/test_Route.py
+++ pyvrp/search/tests/test_Route.py
@@ -1,1 +1,10 @@
-# TODO
+from numpy.testing import assert_, assert_equal
+
+from pyvrp.search._search import Node
+
+
+def test_node_init_not_in_route():
+    node = Node(loc=10)
+    assert_equal(node.client, 10)
+    assert_equal(node.idx, 0)
+    assert_(node.route is None)
