ee799eacba45d2239d6ea8404c3a4da479ed8b13
Niels Wouda <nielswouda@gmail.com>
2023-08-04 12:56:38 +0200

    Update test

--- pyvrp/search/tests/test_Route.py
+++ pyvrp/search/tests/test_Route.py
@@ -1,18 +1,18 @@
+import pytest
 from numpy.testing import assert_, assert_equal
 
 from pyvrp.search._search import Node
 
 
-def test_node_init():
-    node = Node(loc=10)
-    assert_equal(node.client, 10)
+@pytest.mark.parametrize("loc", [0, 1, 10])
+def test_node_init(loc: int):
+    node = Node(loc=loc)
+    assert_equal(node.client, loc)
     assert_equal(node.idx, 0)
     assert_(node.route is None)
 
 
-def test_new_nodes_are_not_depots():
-    node = Node(loc=10)
-    assert_(not node.is_depot())
-
-    node = Node(loc=0)
+@pytest.mark.parametrize("loc", [0, 1, 10])
+def test_new_nodes_are_not_depots(loc: int):
+    node = Node(loc=loc)
     assert_(not node.is_depot())
