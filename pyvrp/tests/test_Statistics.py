db086e1d909a20e54ee74250bcc75296260bd3d5
Niels Wouda <nielswouda@gmail.com>
2023-08-10 10:41:30 +0200

    Add a test for Statistics __eq__

--- pyvrp/tests/test_Statistics.py
+++ pyvrp/tests/test_Statistics.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy.testing import assert_, assert_equal
 
 from pyvrp import CostEvaluator, Population, RandomNumberGenerator, Statistics
@@ -31,3 +32,27 @@ def test_csv_serialises_correctly(tmp_path):
     # as the previously written object.
     read_stats = Statistics.from_csv(csv_path)
     assert_equal(collected_stats, read_stats)
+
+
+@pytest.mark.parametrize("num_iterations", [0, 1, 10])
+def test_eq(num_iterations: int):
+    data = read("data/OkSmall.txt")
+    cost_evaluator = CostEvaluator(20, 6)
+    rng = RandomNumberGenerator(seed=42)
+    pop = Population(broken_pairs_distance)
+
+    for sol in make_random_solutions(10, data, rng):
+        pop.add(sol, cost_evaluator)
+
+    stats = Statistics()
+    for _ in range(num_iterations):  # populate the statistics object
+        stats.collect_from(pop, cost_evaluator)
+
+    assert_equal(stats, stats)
+    assert_(stats != "test")
+
+    if num_iterations > 0:
+        assert_(stats != Statistics())
+
+        assert_equal(stats.feas_stats[0], stats.feas_stats[0])
+        assert_(stats.feas_stats[0] != "test")
