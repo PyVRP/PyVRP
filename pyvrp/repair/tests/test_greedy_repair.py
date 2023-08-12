d6bfdc742a8c2013c652eeb57801f31bbd0cac85
Niels Wouda <nielswouda@gmail.com>
2023-08-12 16:03:28 +0200

    Add another test

--- pyvrp/repair/tests/test_greedy_repair.py
+++ pyvrp/repair/tests/test_greedy_repair.py
@@ -1,21 +1,20 @@
 from numpy.testing import assert_, assert_equal
 
-from pyvrp import CostEvaluator, DynamicBitset, RandomNumberGenerator, Solution
+from pyvrp import CostEvaluator, DynamicBitset, Solution
 from pyvrp.repair import greedy_repair
 from pyvrp.tests.helpers import read
 
 
 def test_empty_unplanned_is_a_no_op():
     data = read("data/OkSmall.txt")
-    rng = RandomNumberGenerator(seed=42)
-
-    sol = Solution.make_random(data, rng)
-    unplanned = DynamicBitset(data.num_clients + 1)
     cost_eval = CostEvaluator(1, 1)
+
+    unplanned = DynamicBitset(data.num_clients + 1)
+    assert_equal(unplanned.count(), 0)
 
     # When unplanned is empty, there is nothing for greedy repair to do, so it
     # should return the exact same solution it received.
-    assert_equal(unplanned.count(), 0)
+    sol = Solution(data, [[3, 2], [1, 4]])
     assert_equal(greedy_repair(sol, unplanned, data, cost_eval), sol)
 
     # This is also true when the solution is not complete: greedy repair only
@@ -23,3 +22,23 @@ def test_empty_unplanned_is_a_no_op():
     sol = Solution(data, [[2, 3, 4]])
     assert_(not sol.is_complete())
     assert_equal(greedy_repair(sol, unplanned, data, cost_eval), sol)
+
+
+def test_after_depot():
+    data = read("data/OkSmall.txt")
+    cost_eval = CostEvaluator(1, 1)
+
+    # We want to insert client 4 into the following single-route solution. It
+    # is optimal to do so directly after the depot, just before client 3.
+    sol = Solution(data, [[3, 2, 1]])
+    unplanned = DynamicBitset(data.num_clients + 1)
+    unplanned[4] = True
+
+    # The greedy repair operator inserts into *existing* routes; it does not
+    # create new ones.
+    repaired = greedy_repair(sol, unplanned, data, cost_eval)
+    assert_equal(sol.num_routes(), repaired.num_routes())
+
+    # Let's check if the repaired solution indeed visits client 4 first.
+    route = repaired.get_routes()[0]
+    assert_equal(route.visits(), [4, 3, 2, 1])
