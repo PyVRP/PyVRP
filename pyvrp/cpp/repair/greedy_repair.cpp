97ccbde8c1357817d097946e777885ebf6865368
Niels Wouda <nielswouda@gmail.com>
2023-08-12 14:33:37 +0200

    Fix greedy repair

--- pyvrp/cpp/repair/greedy_repair.cpp
+++ pyvrp/cpp/repair/greedy_repair.cpp
@@ -9,19 +9,28 @@ using pyvrp::CostEvaluator;
 using pyvrp::DynamicBitset;
 using pyvrp::ProblemData;
 using pyvrp::Solution;
-using pyvrp::search::Exchange;
+using pyvrp::search::n;
 using pyvrp::search::p;
 using pyvrp::search::Route;
 
 using Clients = std::vector<Route::Node>;
 using Routes = std::vector<Route>;
+using TWS = pyvrp::TimeWindowSegment;
 
-void setupRoutes(Solution const &sol,
-                 ProblemData const &data,
-                 Clients &clients,
-                 Routes &routes)
+namespace
+{
+// Populate the given clients and routes vectors with routes from the solution.
+void setupRoutes(Clients &clients,
+                 Routes &routes,
+                 Solution const &sol,
+                 ProblemData const &data)
 {
     assert(clients.empty() && routes.empty());
+
+    // Doing this avoids re-allocations, which would break the pointer structure
+    // that Route and Route::Node use.
+    clients.reserve(data.numClients() + 1);
+    routes.reserve(sol.numRoutes());
 
     for (size_t client = 0; client <= data.numClients(); ++client)
         clients.emplace_back(client);
@@ -30,21 +39,24 @@ void setupRoutes(Solution const &sol,
     for (auto const &solRoute : sol.getRoutes())
     {
         auto &route = routes.emplace_back(data, idx++, solRoute.vehicleType());
-        for (auto const client : solRoute.visits())
+        for (auto const client : solRoute)
             route.push_back(&clients[client]);
-    }
 
-    for (auto &route : routes)
         route.update();
+    }
 }
 
+// Turns the given routes into a solution.
 Solution exportRoutes(ProblemData const &data, Routes const &routes)
 {
     std::vector<Solution::Route> solRoutes;
+    solRoutes.reserve(routes.size());
 
     for (auto const &route : routes)
     {
         std::vector<size_t> visits;
+        visits.reserve(route.size());
+
         for (auto *node : route)
             visits.push_back(node->client());
 
@@ -54,6 +66,43 @@ Solution exportRoutes(ProblemData const &data, Routes const &routes)
     return {data, solRoutes};
 }
 
+// Evaluates the move of inserting U after V.
+pyvrp::Cost evaluate(Route::Node *U,
+                     Route::Node *V,
+                     ProblemData const &data,
+                     CostEvaluator const &costEvaluator)
+{
+    assert(!U->route() && V->route());
+    auto *route = V->route();
+
+    auto const deltaDist = data.dist(V->client(), U->client())
+                           + data.dist(U->client(), n(V)->client())
+                           - data.dist(V->client(), n(V)->client());
+
+    auto const &uClient = data.client(U->client());
+    auto deltaCost = static_cast<pyvrp::Cost>(deltaDist) - uClient.prize;
+
+    deltaCost += costEvaluator.loadPenalty(route->load() + uClient.demand,
+                                           route->capacity());
+    deltaCost -= costEvaluator.loadPenalty(route->load(), route->capacity());
+
+    // If this is true, adding U cannot decrease time warp in V's route enough
+    // to offset the deltaCost.
+    if (deltaCost >= costEvaluator.twPenalty(route->timeWarp()))
+        return deltaCost;
+
+    auto const vTWS = TWS::merge(data.durationMatrix(),
+                                 route->twsBefore(V->idx()),
+                                 TWS(U->client(), uClient),
+                                 route->twsAfter(V->idx() + 1));
+
+    deltaCost += costEvaluator.twPenalty(vTWS.totalTimeWarp());
+    deltaCost -= costEvaluator.twPenalty(route->timeWarp());
+
+    return deltaCost;
+}
+}  // namespace
+
 Solution pyvrp::repair::greedyRepair(Solution const &solution,
                                      DynamicBitset const &unplanned,
                                      ProblemData const &data,
@@ -61,9 +110,7 @@ Solution pyvrp::repair::greedyRepair(Solution const &solution,
 {
     Clients clients;
     Routes routes;
-    setupRoutes(solution, data, clients, routes);
-
-    Exchange<1, 0> move(data);
+    setupRoutes(clients, routes, solution, data);
 
     for (size_t client = 1; client <= data.numClients(); ++client)
     {
@@ -76,24 +123,18 @@ Solution pyvrp::repair::greedyRepair(Solution const &solution,
         Route::Node *UAfter = nullptr;
         pyvrp::Cost deltaCost = std::numeric_limits<pyvrp::Cost>::max();
 
-        for (size_t other = 1; other <= data.numClients(); ++other)
+        for (auto &route : routes)
         {
-            if (unplanned[other] || other == client)
-                continue;
-
-            auto *V = &clients[other];
-            assert(V->route());
-
-            auto const cost = move.evaluate(U, V, costEvaluator);
-            if (cost < deltaCost)
+            auto const cost = evaluate(U, route[0], data, costEvaluator);
+            if (cost < deltaCost)  // evaluate after depot
             {
                 deltaCost = cost;
-                UAfter = V;
+                UAfter = route[0];
             }
 
-            if (p(V)->isDepot())
+            for (auto *V : route)  // evaluate after V
             {
-                auto const cost = move.evaluate(U, p(V), costEvaluator);
+                auto const cost = evaluate(U, V, data, costEvaluator);
                 if (cost < deltaCost)
                 {
                     deltaCost = cost;
@@ -102,7 +143,8 @@ Solution pyvrp::repair::greedyRepair(Solution const &solution,
             }
         }
 
-        move.apply(U, UAfter);
+        assert(UAfter && UAfter->route());
+        UAfter->route()->insert(UAfter->idx() + 1, U);
         UAfter->route()->update();
     }
 
