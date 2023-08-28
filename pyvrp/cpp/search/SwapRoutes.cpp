f8f85f066946499e2d426991d51434502e5b1993
Niels Wouda <nielswouda@gmail.com>
2023-08-28 22:45:30 +0200

    Finish SwapRoutes operator

--- pyvrp/cpp/search/SwapRoutes.cpp
+++ pyvrp/cpp/search/SwapRoutes.cpp
@@ -23,17 +23,51 @@ Cost SwapRoutes::evaluate(Route *U,
     deltaCost += costEvaluator.loadPenalty(V->load(), U->capacity());
     deltaCost -= costEvaluator.loadPenalty(V->load(), V->capacity());
 
+    auto const &uVehicleType = data.vehicleType(U->vehicleType());
+    auto const &vVehicleType = data.vehicleType(V->vehicleType());
+
+    auto const uDepot = uVehicleType.depot;
+    auto const vDepot = vVehicleType.depot;
+
+    if (uDepot == vDepot)
+        // Then the start and end depots are the same, so there are no changes
+        // in distance or time warp, and we do not have to evaluate those.
+        return deltaCost;
+
     // Changes in distance from and to the depot.
-    // TODO
+    auto const uDeltaDist = data.dist(vDepot, (*U->begin())->client())
+                            + data.dist((*U->end())->client(), vDepot)
+                            - data.dist(uDepot, (*U->begin())->client())
+                            - data.dist((*U->end())->client(), uDepot);
+
+    auto const vDeltaDist = data.dist(uDepot, (*V->begin())->client())
+                            + data.dist((*V->end())->client(), uDepot)
+                            - data.dist(vDepot, (*V->begin())->client())
+                            - data.dist((*V->end())->client(), vDepot);
+
+    deltaCost += static_cast<Cost>(uDeltaDist) + static_cast<Cost>(vDeltaDist);
 
     // Changes in time warp.
-    // TODO
+    auto const uTWS = TWS::merge(data.durationMatrix(),
+                                 V->tws(0),
+                                 U->twsBetween(1, U->size()),
+                                 V->tws(V->size() + 1));
+
+    deltaCost += costEvaluator.twPenalty(uTWS.totalTimeWarp());
+    deltaCost -= costEvaluator.twPenalty(U->timeWarp());
+
+    auto const vTWS = TWS::merge(data.durationMatrix(),
+                                 U->tws(0),
+                                 V->twsBetween(1, V->size()),
+                                 U->tws(U->size() + 1));
+
+    deltaCost += costEvaluator.twPenalty(vTWS.totalTimeWarp());
+    deltaCost -= costEvaluator.twPenalty(V->timeWarp());
 
     return deltaCost;
 }
 
-void SwapRoutes::apply([[maybe_unused]] Route *U,
-                       [[maybe_unused]] Route *V) const
+void SwapRoutes::apply(Route *U, Route *V) const
 {
     std::vector<Route::Node *> uNodes = {U->begin(), U->end()};
     std::vector<Route::Node *> vNodes = {V->begin(), V->end()};
