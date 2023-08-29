7ff8a6b0eba0ee8bfb29e3b295e0408e461cdb33
Niels Wouda <nielswouda@gmail.com>
2023-08-29 12:47:11 +0200

    Remove unused evaluation logic from SwapRoutes

--- pyvrp/cpp/search/SwapRoutes.cpp
+++ pyvrp/cpp/search/SwapRoutes.cpp
@@ -34,36 +34,8 @@ Cost SwapRoutes::evaluate(Route *U,
         // in distance or time warp, and we do not have to evaluate those.
         return deltaCost;
 
-    // Changes in distance from and to the depot.
-    auto const uDeltaDist = data.dist(vDepot, (*U->begin())->client())
-                            + data.dist((*U->end())->client(), vDepot)
-                            - data.dist(uDepot, (*U->begin())->client())
-                            - data.dist((*U->end())->client(), uDepot);
-
-    auto const vDeltaDist = data.dist(uDepot, (*V->begin())->client())
-                            + data.dist((*V->end())->client(), uDepot)
-                            - data.dist(vDepot, (*V->begin())->client())
-                            - data.dist((*V->end())->client(), vDepot);
-
-    deltaCost += static_cast<Cost>(uDeltaDist) + static_cast<Cost>(vDeltaDist);
-
-    // Changes in time warp.
-    auto const uTWS = TWS::merge(data.durationMatrix(),
-                                 V->tws(0),
-                                 U->twsBetween(1, U->size()),
-                                 V->tws(V->size() + 1));
-
-    deltaCost += costEvaluator.twPenalty(uTWS.totalTimeWarp());
-    deltaCost -= costEvaluator.twPenalty(U->timeWarp());
-
-    auto const vTWS = TWS::merge(data.durationMatrix(),
-                                 U->tws(0),
-                                 V->twsBetween(1, V->size()),
-                                 U->tws(U->size() + 1));
-
-    deltaCost += costEvaluator.twPenalty(vTWS.totalTimeWarp());
-    deltaCost -= costEvaluator.twPenalty(V->timeWarp());
-
+    // TODO handle the case of depot differences (multiple depots). There is
+    // some evaluation code for this in issue #188.
     return deltaCost;
 }
 
