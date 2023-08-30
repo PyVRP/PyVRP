686f7080d6b78768ecab28cbfad4a35b12f74c8f
Leon Lan <l.lan@vu.nl>
2023-08-30 11:38:13 +0200

    Add napoleon back in; exclude Python attr and params from TOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ autodoc_typehints = "signature"
 numpydoc_xref_param_type = True
 numpydoc_class_members_toctree = False
 numpydoc_attributes_as_param_list = False
+napoleon_include_special_with_doc = True
 
 # -- nbsphinx
 nbsphinx_execute = "always"
@@ -40,6 +41,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
     "sphinx_immaterial",
     "nbsphinx",
     "numpydoc",
@@ -136,7 +138,10 @@ python_transform_type_annotations_pep585 = True
 python_transform_type_annotations_pep604 = True
 object_description_options = [
     ("py:.*", dict(include_fields_in_toc=False, include_rubrics_in_toc=False)),
+    ("py:attribute", dict(include_in_toc=False)),
+    ("py:parameter", dict(include_in_toc=False)),
 ]
+
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
