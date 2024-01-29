67f03324c52da7fa4ee16c58a9c3fa9842f61d55
Niels Wouda <nielswouda@gmail.com>
2024-01-29 17:15:12 +0100

    Link to v0.7.0 documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -159,6 +159,11 @@ html_theme_options = {
             "aliases": [],
         },
         {
+            "version": "https://pyvrp.github.io/v0.7.0",
+            "title": "v0.7.0",
+            "aliases": [],
+        },
+        {
             "version": "https://pyvrp.github.io/v0.6.0",
             "title": "v0.6.0",
             "aliases": [],
