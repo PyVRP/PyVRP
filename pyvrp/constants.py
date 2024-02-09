8922a02319229d133e23bae3d5d3973e7d7acd87
Niels Wouda <N-Wouda@users.noreply.github.com>
2024-02-09 15:44:03 +0100

    Add asserts that check for overflow (#462)

--- pyvrp/constants.py
+++ pyvrp/constants.py
@@ -1,2 +1,11 @@
-MAX_USER_VALUE = 2 << 25
-MAX_VALUE = 2 << 28
+MAX_USER_VALUE = 2 << 22
+"""
+Maximum input value the user may provide. Any bigger value issues a warning
+about possible scaling issues.
+"""
+
+MAX_VALUE = 2 << 25
+"""
+The default maximum value that is used internally when an edge is missing in
+the input data. 
+"""
