45378651531a0c91d4e2b60f9a98cd44cfc249c5
Leon Lan <l.lan@vu.nl>
2024-02-22 18:01:42 +0100

    Decrease constant max values

--- pyvrp/constants.py
+++ pyvrp/constants.py
@@ -1,10 +1,10 @@
-MAX_USER_VALUE = 2 << 22
+MAX_USER_VALUE = 2 << 20
 """
 Maximum input value the user may provide. Any bigger value issues a warning
 about possible scaling issues.
 """
 
-MAX_VALUE = 2 << 25
+MAX_VALUE = 2 << 23
 """
 The default maximum value that is used internally when an edge is missing in
 the input data. 
