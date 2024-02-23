ae29aa71307a7de031847e982b2eb5b2dafa2b53
Leon Lan <l.lan@vu.nl>
2024-02-23 16:08:27 +0100

    Revert constants changes

--- pyvrp/constants.py
+++ pyvrp/constants.py
@@ -1,10 +1,10 @@
-MAX_USER_VALUE = 2 << 21
+MAX_USER_VALUE = 2 << 22
 """
 Maximum input value the user may provide. Any bigger value issues a warning
 about possible scaling issues.
 """
 
-MAX_VALUE = 2 << 24
+MAX_VALUE = 2 << 25
 """
 The default maximum value that is used internally when an edge is missing in
 the input data. 
