61a987250e2f56ab55c75c97547263f2d629664b
Leon Lan <l.lan@vu.nl>
2024-02-23 14:50:02 +0100

    Increase a bit

--- pyvrp/constants.py
+++ pyvrp/constants.py
@@ -1,10 +1,10 @@
-MAX_USER_VALUE = 2 << 20
+MAX_USER_VALUE = 2 << 21
 """
 Maximum input value the user may provide. Any bigger value issues a warning
 about possible scaling issues.
 """
 
-MAX_VALUE = 2 << 23
+MAX_VALUE = 2 << 24
 """
 The default maximum value that is used internally when an edge is missing in
 the input data. 
