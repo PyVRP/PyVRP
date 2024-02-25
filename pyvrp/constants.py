fb73d00f906aed9221db3b94dc98249ab03211ca
Niels Wouda <nielswouda@gmail.com>
2024-02-25 18:51:22 +0100

    Switch to assert_allclose in older tests to avoid small precision issues

--- pyvrp/constants.py
+++ pyvrp/constants.py
@@ -1,10 +1,10 @@
-MAX_USER_VALUE = 2 << 48
+MAX_USER_VALUE = 1 << 48
 """
 Maximum input value the user may provide. Any bigger value issues a warning
 about possible scaling issues.
 """
 
-MAX_VALUE = 2 << 52
+MAX_VALUE = 1 << 52
 """
 The default maximum value that is used internally when an edge is missing in
 the input data. 
