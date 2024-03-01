6708d77b1a65283460970f11a6eb2450004ba9ca
Niels Wouda <nielswouda@gmail.com>
2024-03-01 17:25:47 +0100

    Add more tests

--- tests/test_CostEvaluator.py
+++ tests/test_CostEvaluator.py
@@ -29,27 +29,20 @@ def test_load_penalty():
     assert_allclose(cost_evaluator.load_penalty(3, 1), 8)
 
 
-@mark.parametrize("capacity", [5, 15, 29, 51, 103])
-def test_load_penalty_always_zero_when_below_capacity(capacity: int):
+@mark.parametrize("cap", [5, 15, 29, 51, 103])
+def test_load_penalty_always_zero_when_below_capacity(cap: int):
     """
     This test asserts that load penalties are only applied to excess load, that
     is, load in excess of the vehicle's capacity.
     """
-    load_penalty = 2
-    cost_evaluator = CostEvaluator(load_penalty, 1, 0)
+    penalty = 2
+    cost_eval = CostEvaluator(penalty, 1, 0)
 
-    for load in range(capacity):  # all below capacity
-        assert_allclose(cost_evaluator.load_penalty(load, capacity), 0)
-
-    # at capacity
-    assert_allclose(cost_evaluator.load_penalty(capacity, capacity), 0)
-    # above capacity
-    assert_allclose(
-        cost_evaluator.load_penalty(capacity + 1, capacity), load_penalty
-    )
-    assert_allclose(
-        cost_evaluator.load_penalty(capacity + 2, capacity), 2 * load_penalty
-    )
+    assert_allclose(cost_eval.load_penalty(0, cap), 0)  # below cap
+    assert_allclose(cost_eval.load_penalty(cap - 1, cap), 0)
+    assert_allclose(cost_eval.load_penalty(cap, cap), 0)  # at cap
+    assert_allclose(cost_eval.load_penalty(cap + 1, cap), penalty)  # above cap
+    assert_allclose(cost_eval.load_penalty(cap + 2, cap), 2 * penalty)
 
 
 def test_tw_penalty():
@@ -58,17 +51,38 @@ def test_tw_penalty():
     """
     cost_evaluator = CostEvaluator(1, 2, 0)
 
-    # Penalty per unit time warp is 2
+    # Penalty per unit time warp is 2.
     assert_allclose(cost_evaluator.tw_penalty(0), 0)
     assert_allclose(cost_evaluator.tw_penalty(1), 2)
     assert_allclose(cost_evaluator.tw_penalty(2), 4)
 
     cost_evaluator = CostEvaluator(1, 4, 0)
 
-    # Penalty per unit excess capacity is now 4
+    # Penalty per unit excess capacity is now 4.
     assert_allclose(cost_evaluator.tw_penalty(0), 0)
     assert_allclose(cost_evaluator.tw_penalty(1), 4)
     assert_allclose(cost_evaluator.tw_penalty(2), 8)
+
+
+def test_dist_penalty():
+    """
+    This test asserts that excess distance penalty computations are correct.
+    """
+    cost_eval = CostEvaluator(1, 1, 2)
+
+    # Penalty per unit excess distance is 2.
+    assert_allclose(cost_eval.dist_penalty(-1, 0), 0)
+    assert_allclose(cost_eval.dist_penalty(0, 0), 0)
+    assert_allclose(cost_eval.dist_penalty(1, 0), 2)
+    assert_allclose(cost_eval.dist_penalty(2, 0), 4)
+
+    cost_eval = CostEvaluator(1, 1, 4)
+
+    # Penalty per unit excess capacity is now 4.
+    assert_allclose(cost_eval.dist_penalty(-1, 0), 0)
+    assert_allclose(cost_eval.dist_penalty(0, 0), 0)
+    assert_allclose(cost_eval.dist_penalty(1, 0), 4)
+    assert_allclose(cost_eval.dist_penalty(2, 0), 8)
 
 
 def test_cost(ok_small):
@@ -154,6 +168,30 @@ def test_penalised_cost(ok_small):
 
     # Default cost evaluator has 0 weights and only computes distance as cost
     assert_allclose(default_evaluator.penalised_cost(infeas), infeas_dist)
+
+
+def test_excess_distance_penalised_cost(ok_small):
+    """
+    Tests that excess distance is properly penalised in the cost computations.
+    """
+    vehicle_type = VehicleType(3, capacity=10, max_distance=5_000)
+    data = ok_small.replace(vehicle_types=[vehicle_type])
+
+    sol = Solution(data, [[1, 2], [3, 4]])
+    assert_(not sol.is_feasible())
+
+    routes = sol.routes()
+
+    assert_allclose(sol.distance(), 5501 + 4224)
+    assert_allclose(routes[0].distance(), 5501)
+    assert_allclose(routes[1].distance(), 4224)
+
+    assert_allclose(sol.excess_distance(), 501)
+    assert_allclose(routes[0].excess_distance(), 501)
+    assert_allclose(routes[1].excess_distance(), 0)
+
+    cost_eval = CostEvaluator(0, 0, 10)
+    assert_allclose(cost_eval.penalised_cost(sol), 5501 + 4224 + 10 * 501)
 
 
 @mark.parametrize(
