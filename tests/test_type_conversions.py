749bf908da3083c4d8f41467da1ae79c9dcdc7bc
Niels Wouda <N-Wouda@users.noreply.github.com>
2024-03-25 16:30:22 +0100

    Fix Windows wheels, harden CD to also execute tests with built wheels (#548)

---
+++ tests/test_type_conversions.py
@@ -0,0 +1,45 @@
+__doc__ = """
+The tests in this file check that Python objects convertible to integers are
+handled appropriately on the C++ side - this includes checking for potential
+overflows and meaningful responses in that case.
+"""
+
+import numpy as np
+import pytest
+from numpy.testing import assert_equal, assert_raises
+
+from pyvrp import Depot
+
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        (1, 1),
+        (0, 0),
+        ("1", 1),
+        (1.0, 1),
+        (1.5, 1),
+        (2**31 - 1, 2**31 - 1),
+        (2**31, 2**31),
+        (2**63 - 1, 2**63 - 1),  # largest supported 64-bit signed value
+    ],
+)
+def test_measure_convertible_to_int(input, expected):
+    """
+    Tests that input argument for which int() succeeds are also properly
+    handled on the C++ side.
+    """
+    depot = Depot(1, 1, tw_late=input)
+    assert_equal(depot.tw_late, expected)
+
+
+@pytest.mark.parametrize(
+    "input", [np.iinfo(np.int64).max + 1, np.finfo(np.float64).max]
+)
+def test_larger_than_max_size(input):
+    """
+    Tests that arguments larger than the maximum value for measure arguments
+    raise an overflow error to warn the user.
+    """
+    with assert_raises(OverflowError):
+        Depot(1, 1, tw_late=input)
