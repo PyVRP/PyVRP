5e9ef2ca6e809e4cc74ef7cd8261e94b8532f319
Leon Lan <l.lan@vu.nl>
2024-05-03 16:50:43 +0200

    Install dependencies

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-git submodule update --init
+# Add ~/.local/bin to PATH for pip-installed executables
+echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
+
+# Install g++-13
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install g++-13
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+
+# Install recent CMake version
+pipx install cmake
+
+# Install poetry
+pipx install poetry
+
+# Install package dependencies
+poetry install
