5238532501789918f7589781042feb6c6b23dcca
Leon Lan <l.lan@vu.nl>
2024-05-03 17:35:17 +0200

    Downgrade to g++11

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,11 +3,11 @@
 # Add ~/.local/bin to PATH for pip-installed executables
 echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
 
-# Install g++-13
+# Install g++-11
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt update
-sudo apt install g++-13
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+sudo apt install g++-11
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
 
 # Install recent CMake version
 pipx install cmake
