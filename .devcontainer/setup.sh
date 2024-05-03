925bb33a8a9e19fb9711ba9f650e2d1d495b798d
Leon Lan <l.lan@vu.nl>
2024-05-03 17:35:43 +0200

    Remove cmake

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Add ~/.local/bin to PATH for pip-installed executables
-echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
-
 # Install g++-11
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install g++-11
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
-
-# Install recent CMake version
-pipx install cmake
 
 # Install poetry
 pipx install poetry
