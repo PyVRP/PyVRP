e588b471182f50c9206d49472f9bab00dae00c51
Leon Lan <l.lan@vu.nl>
2024-05-06 14:34:16 +0200

    Install poetry without pipx

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,10 +6,5 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.10 python3-pip
 
-# Install pipx
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-sudo pipx ensurepath --global
-
 # Install poetry
-pipx install poetry
+curl -sSL https://install.python-poetry.org | python3 -
