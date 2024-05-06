e8dd3d05af70aea5ebe8c9deb08a7585eb9ee571
Leon Lan <l.lan@vu.nl>
2024-05-06 14:29:49 +0200

    Install python3-pip

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Install Python3.10
+# Install Python and pip
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.10
+sudo apt-get install python3.10 python3-pip
 
-# Install poetry
+# Install pipx
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 sudo pipx ensurepath --global
+
+# Install poetry
 pipx install poetry
