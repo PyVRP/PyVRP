87ddcd56107c47f97b0950fc4f74e435dd852fe4
Niels Wouda <nielswouda@gmail.com>
2024-07-20 12:39:14 +0200

    Raise when multiple vehicle types are present

--- pyvrp/minimise_fleet.py
+++ pyvrp/minimise_fleet.py
@@ -1,4 +1,4 @@
-from collections import Counter
+import numpy as np
 
 from pyvrp._pyvrp import ProblemData, VehicleType
 from pyvrp.solve import SolveParams, solve
@@ -10,10 +10,16 @@ def minimise_fleet(
     stop: StoppingCriterion,
     seed: int = 0,
     params: SolveParams = SolveParams(),
-) -> list[VehicleType]:
+) -> VehicleType:
     """
     Attempts to reduce the number of vehicles needed to achieve a feasible
     solution to the given problem instance, subject to a stopping criterion.
+
+    .. warning::
+
+       This function is currently unable to solve instances with multiple
+       vehicle types. Support for such a setting may be added in future
+       versions of PyVRP.
 
     Parameters
     ----------
@@ -29,18 +35,29 @@ def minimise_fleet(
 
     Returns
     -------
-    list[VehicleType]
+    VehicleType
         The smallest fleet composition admitting a feasible solution to the
         problem instances that could be found before the stopping criterion was
         hit. The original fleet is returned if no feasible solution was found.
-    """
-    feas_fleet = data.vehicle_types()
 
-    while True:
-        # TODO which vehicle to take out?
-        fleet = feas_fleet
-        fleet = _vehicles({0: fleet[0].num_available - 1}, fleet)
-        data = data.replace(vehicle_types=fleet)
+    Raises
+    ------
+    ValueError
+        When the instance contains more than one vehicle type. That setting is
+        not yet supported.
+    """
+    if data.num_vehicle_types != 1:
+        msg = "Fleet minimisation does not understand multiple vehicle types."
+        raise ValueError(msg)
+
+    feas_fleet = data.vehicle_type(0)
+    lower_bound = _lower_bound(data)
+
+    while feas_fleet.num_available > lower_bound:
+        # Reduce from feasible fleet by one vehicle, and retry solving that
+        # reduced instance.
+        fleet = _vehicles(feas_fleet.num_available - 1, feas_fleet)
+        data = data.replace(vehicle_types=[fleet])
 
         res = solve(
             data,
@@ -54,34 +71,43 @@ def minimise_fleet(
         if stop(res.cost()) or not res.is_feasible():
             return feas_fleet
 
-        # TODO we can also quit if we have hit a lower bound. See e.g.
-        # https://hal.science/hal-00992081/document for ideas
-
         feas_fleet = fleet
         if res.best.num_routes() < data.num_vehicles:
             # Then we can make a bigger jump because more than one vehicle of
             # the feasible fleet was unused.
-            used_types = Counter(r.vehicle_type() for r in res.best.routes())
-            feas_fleet = _vehicles(used_types, fleet)
+            feas_fleet = _vehicles(res.best.num_routes(), fleet)
+
+    return feas_fleet
 
 
-def _vehicles(type_counts, vehicle_types):
-    return [
-        VehicleType(
-            type_counts[vehicle_type],
-            veh_type.capacity,
-            veh_type.start_depot,
-            veh_type.end_depot,
-            veh_type.fixed_cost,
-            veh_type.tw_early,
-            veh_type.tw_late,
-            veh_type.max_duration,
-            veh_type.max_distance,
-            veh_type.unit_distance_cost,
-            veh_type.unit_duration_cost,
-            veh_type.profile,
-            name=veh_type.name,
-        )
-        for vehicle_type, veh_type in zip(type_counts, vehicle_types)
-        if type_counts[vehicle_type] > 0
-    ]
+def _vehicles(num_available: int, vehicle_type: VehicleType) -> VehicleType:
+    return VehicleType(
+        num_available,
+        vehicle_type.capacity,
+        vehicle_type.start_depot,
+        vehicle_type.end_depot,
+        vehicle_type.fixed_cost,
+        vehicle_type.tw_early,
+        vehicle_type.tw_late,
+        vehicle_type.max_duration,
+        vehicle_type.max_distance,
+        vehicle_type.unit_distance_cost,
+        vehicle_type.unit_duration_cost,
+        vehicle_type.profile,
+        name=vehicle_type.name,
+    )
+
+
+def _lower_bound(data: ProblemData) -> int:
+    vehicle_type = data.vehicle_type(0)
+
+    # TODO additional simple bounding techniques exist, for example based on
+    # time properties. See https://doi.org/10.1007/978-3-319-07046-9_30 for
+    # details.
+
+    # Computes a simple bound based on packing delivery or pickup demand in the
+    # given vehicles.
+    delivery = sum(c.delivery for c in data.clients())
+    pickup = sum(c.pickup for c in data.clients())
+    demand = max(delivery, pickup)
+    return int(np.ceil(demand / max(vehicle_type.capacity, 1)))
--- tests/test_minimise_fleet.py
+++ tests/test_minimise_fleet.py
@@ -1,8 +1,19 @@
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_raises
 
 from pyvrp import minimise_fleet
 from pyvrp.stop import MaxIterations
 from tests.helpers import read
+
+
+def test_raises_multiple_vehicle_types(ok_small_multi_depot):
+    """
+    Tests that ``minimise_fleet`` raises when given an instance with multiple
+    vehicle types.
+    """
+    assert_equal(ok_small_multi_depot.num_vehicle_types, 2)
+
+    with assert_raises(ValueError):
+        minimise_fleet(ok_small_multi_depot, MaxIterations(1))
 
 
 def test_OkSmall(ok_small):
@@ -12,8 +23,8 @@ def test_OkSmall(ok_small):
     """
     assert_equal(ok_small.num_vehicles, 3)
 
-    veh_types = minimise_fleet(ok_small, MaxIterations(10))
-    data = ok_small.replace(vehicle_types=veh_types)
+    vehicle_type = minimise_fleet(ok_small, MaxIterations(10))
+    data = ok_small.replace(vehicle_types=[vehicle_type])
     assert_equal(data.num_vehicles, 2)
 
 
@@ -24,8 +35,8 @@ def test_rc208(rc208):
     """
     assert_equal(rc208.num_vehicles, 25)
 
-    veh_types = minimise_fleet(rc208, MaxIterations(10))
-    data = rc208.replace(vehicle_types=veh_types)
+    vehicle_type = minimise_fleet(rc208, MaxIterations(10))
+    data = rc208.replace(vehicle_types=[vehicle_type])
     assert_equal(data.num_vehicles, 5)
 
 
@@ -37,6 +48,6 @@ def test_X_instance():
     data = read("data/X-n101-50-k13.vrp", round_func="round")
     assert_equal(data.num_vehicles, 100)
 
-    veh_types = minimise_fleet(data, MaxIterations(10))
-    data = data.replace(vehicle_types=veh_types)
+    vehicle_type = minimise_fleet(data, MaxIterations(10))
+    data = data.replace(vehicle_types=[vehicle_type])
     assert_equal(data.num_vehicles, 14)
