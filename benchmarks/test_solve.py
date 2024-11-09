c938abdd3567d70be710eb8d690510010fdcbf55
Niels Wouda <nielswouda@gmail.com>
2024-11-09 20:07:55 +0100

    Add additional solve benchmark

--- benchmarks/test_solve.py
+++ benchmarks/test_solve.py
@@ -5,9 +5,18 @@ from pyvrp.stop import MaxIterations
 
 
 @pytest.mark.parametrize("instance", ["vrptw", "mdvrp", "vrpb"])
-def test_solve(instance, benchmark, request):
+def test_solve_one_iteration(instance, benchmark, request):
     """
-    Tests performance of solving various instances.
+    Tests performance of solving various instances using one iteration.
     """
     data = request.getfixturevalue(instance)
     benchmark(solve, data, stop=MaxIterations(1), seed=0)
+
+
+@pytest.mark.parametrize("instance", ["vrptw", "mdvrp", "vrpb"])
+def test_solve_more_iterations(instance, benchmark, request):
+    """
+    Tests performance of solving various instances over several iterations.
+    """
+    data = request.getfixturevalue(instance)
+    benchmark(solve, data, stop=MaxIterations(10), seed=0)
