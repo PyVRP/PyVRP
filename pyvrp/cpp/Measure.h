56af91fc0d296d7992f17d20613ee70afca0d13c
Niels Wouda <nielswouda@gmail.com>
2024-11-18 11:16:41 +0100

    Generalise Measure to handle different value types

--- pyvrp/cpp/Measure.h
+++ pyvrp/cpp/Measure.h
@@ -21,10 +21,11 @@ enum class MeasureType
     DURATION,
     COST,
     LOAD,
+    INDEX,
 };
 
 // Forward declaration so we can define the relevant type aliases early.
-template <MeasureType _> class Measure;
+template <MeasureType _, typename ValueType = Value> class Measure;
 
 // Type aliases. These are used throughout the program.
 using Coordinate = Measure<MeasureType::COORD>;
@@ -32,29 +33,30 @@ using Cost = Measure<MeasureType::COST>;
 using Distance = Measure<MeasureType::DIST>;
 using Duration = Measure<MeasureType::DURATION>;
 using Load = Measure<MeasureType::LOAD>;
+using Index = Measure<MeasureType::INDEX, uint32_t>;
 
 //
 //                 EVERYTHING BELOW THIS IS IMPLEMENTATION
 //
 
 /**
- * The measure class is a thin wrapper around an underlying ``Value``. The
+ * The measure class is a thin wrapper around an underlying ``ValueType``. The
  * measure forms a strong type that is only explicitly convertible into other
  * arithmetic or measure types.
  */
-template <MeasureType _> class Measure
+template <MeasureType _, typename ValueType> class Measure
 {
-    Value value;
+    ValueType value;
 
 public:
     // Default construction initialises to 0.
     Measure() = default;
 
     // This constructor takes any arithmetic type (generally useful) and casts
-    // its value to Value.
+    // its value to ValueType.
     template <typename T,
               std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
-    Measure(T const value) : value(static_cast<Value>(value))
+    Measure(T const value) : value(static_cast<ValueType>(value))
     {
     }
 
@@ -73,7 +75,7 @@ public:
     }
 
     // Retreives the underlying value.
-    [[nodiscard]] Value get() const;
+    [[nodiscard]] ValueType get() const;
 
     // In-place unary operators.
     Measure &operator+=(Measure const &rhs);
@@ -87,134 +89,169 @@ public:
 };
 
 // Retreives the underlying value.
-template <MeasureType Type> Value Measure<Type>::get() const { return value; }
+template <MeasureType Type, typename ValueType>
+ValueType Measure<Type, ValueType>::get() const
+{
+    return value;
+}
 
 // In-place unary operators.
-template <MeasureType Type>
-Measure<Type> &Measure<Type>::operator+=(Measure<Type> const &rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> &
+Measure<Type, ValueType>::operator+=(Measure<Type, ValueType> const &rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_add_overflow(this->value, rhs.value, &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_add_overflow(this->value, rhs.value, &res));
+    }
 
     this->value += rhs.value;
     return *this;
 }
 
-template <MeasureType Type>
-Measure<Type> &Measure<Type>::operator-=(Measure<Type> const &rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> &
+Measure<Type, ValueType>::operator-=(Measure<Type, ValueType> const &rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_sub_overflow(this->value, rhs.value, &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_sub_overflow(this->value, rhs.value, &res));
+    }
 
     this->value -= rhs.value;
     return *this;
 }
 
-template <MeasureType Type>
-Measure<Type> &Measure<Type>::operator*=(Measure<Type> const &rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> &
+Measure<Type, ValueType>::operator*=(Measure<Type, ValueType> const &rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_mul_overflow(this->value, rhs.value, &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_mul_overflow(this->value, rhs.value, &res));
+    }
 
     this->value *= rhs.value;
     return *this;
 }
 
-template <MeasureType Type>
-Measure<Type> &Measure<Type>::operator/=(Measure<Type> const &rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> &
+Measure<Type, ValueType>::operator/=(Measure<Type, ValueType> const &rhs)
 {
     this->value /= rhs.value;
     return *this;
 }
 
 // Comparison operators.
-template <MeasureType Type>
-bool Measure<Type>::operator==(Measure<Type> const &other) const
+template <MeasureType Type, typename ValueType>
+bool Measure<Type, ValueType>::operator==(
+    Measure<Type, ValueType> const &other) const
 {
     return value == other.value;
 }
 
-template <MeasureType Type>
-std::strong_ordering
-Measure<Type>::operator<=>(Measure<Type> const &other) const
+template <MeasureType Type, typename ValueType>
+std::strong_ordering Measure<Type, ValueType>::operator<=>(
+    Measure<Type, ValueType> const &other) const
 {
     return value <=> other.value;
 }
 
 // Free-standing binary operators.
-template <MeasureType Type>
-Measure<Type> operator+(Measure<Type> const lhs, Measure<Type> const rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> operator+(Measure<Type, ValueType> const lhs,
+                                   Measure<Type, ValueType> const rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_add_overflow(lhs.get(), rhs.get(), &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_add_overflow(lhs.get(), rhs.get(), &res));
+    }
 
     return lhs.get() + rhs.get();
 }
 
-template <MeasureType Type> Measure<Type> operator+(Measure<Type> const lhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type> operator+(Measure<Type> const lhs)
 {
     return +lhs.get();
 }
 
-template <MeasureType Type>
-Measure<Type> operator-(Measure<Type> const lhs, Measure<Type> const rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> operator-(Measure<Type, ValueType> const lhs,
+                                   Measure<Type, ValueType> const rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_sub_overflow(lhs.get(), rhs.get(), &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_sub_overflow(lhs.get(), rhs.get(), &res));
+    }
 
     return lhs.get() - rhs.get();
 }
 
-template <MeasureType Type> Measure<Type> operator-(Measure<Type> const lhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> operator-(Measure<Type, ValueType> const lhs)
 {
     return -lhs.get();
 }
 
-template <MeasureType Type>
-Measure<Type> operator*(Measure<Type> const lhs, Measure<Type> const rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> operator*(Measure<Type, ValueType> const lhs,
+                                   Measure<Type, ValueType> const rhs)
 {
-    [[maybe_unused]] Value res = 0;
-    assert(!__builtin_mul_overflow(lhs.get(), rhs.get(), &res));
+    if constexpr (std::is_signed_v<ValueType>)
+    {
+        [[maybe_unused]] ValueType res = 0;
+        assert(!__builtin_mul_overflow(lhs.get(), rhs.get(), &res));
+    }
 
     return lhs.get() * rhs.get();
 }
 
-template <MeasureType Type>
-Measure<Type> operator/(Measure<Type> const lhs, Measure<Type> const rhs)
+template <MeasureType Type, typename ValueType>
+Measure<Type, ValueType> operator/(Measure<Type, ValueType> const lhs,
+                                   Measure<Type, ValueType> const rhs)
 {
     return lhs.get() / rhs.get();
 }
 }  // namespace pyvrp
 
 // For printing.
-template <pyvrp::MeasureType Type>
-std::ostream &operator<<(std::ostream &out, pyvrp::Measure<Type> const measure)
+template <pyvrp::MeasureType Type, typename ValueType>
+std::ostream &operator<<(std::ostream &out,
+                         pyvrp::Measure<Type, ValueType> const measure)
 {
     return out << measure.get();
 }
 
 // Specialisations for hashing and numerical limits.
 
-template <pyvrp::MeasureType Type> struct std::hash<pyvrp::Measure<Type>>
+template <pyvrp::MeasureType Type, typename ValueType>
+struct std::hash<pyvrp::Measure<Type, ValueType>>
 {
-    size_t operator()(pyvrp::Measure<Type> const measure) const
+    size_t operator()(pyvrp::Measure<Type, ValueType> const measure) const
     {
-        return std::hash<pyvrp::Value>()(measure.get());
+        return std::hash<ValueType>()(measure.get());
     }
 };
 
-template <pyvrp::MeasureType Type>
-class std::numeric_limits<pyvrp::Measure<Type>>
+template <pyvrp::MeasureType Type, typename ValueType>
+class std::numeric_limits<pyvrp::Measure<Type, ValueType>>
 {
 public:
-    static pyvrp::Measure<Type> max()
+    static pyvrp::Measure<Type, ValueType> max()
     {
-        return std::numeric_limits<pyvrp::Value>::max();
+        return std::numeric_limits<ValueType>::max();
     }
 
-    static pyvrp::Measure<Type> min()
+    static pyvrp::Measure<Type, ValueType> min()
     {
-        return std::numeric_limits<pyvrp::Value>::min();
+        return std::numeric_limits<ValueType>::min();
     }
 };
 
