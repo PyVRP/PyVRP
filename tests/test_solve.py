e24537cc53e37d148b94b629ef5e806ad1363be4
Niels Wouda <nielswouda@gmail.com>
2024-12-06 11:08:02 +0100

    Phrasing

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -79,8 +79,8 @@ def test_solve_more_iterations_is_better(prize_collecting):
     solution.
     """
     cost1 = solve(prize_collecting, stop=MaxIterations(1)).cost()
-    cost100 = solve(prize_collecting, stop=MaxIterations(10)).cost()
-    assert_(cost100 < cost1)
+    cost10 = solve(prize_collecting, stop=MaxIterations(10)).cost()
+    assert_(cost10 < cost1)
 
 
 def test_solve_custom_params(ok_small):
