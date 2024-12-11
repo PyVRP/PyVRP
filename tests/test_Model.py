5ba5dc00e454db81902e0a5be89731a3cae40373
Joop van der Heiden <138480238+joopvdh@users.noreply.github.com>
2024-12-11 10:35:44 +0100

    Add another multi-trip unit test in test_Model

--- tests/test_Model.py
+++ tests/test_Model.py
@@ -937,10 +937,10 @@ def test_model_solves_instance_with_zero_load_dimensions():
     assert_equal(route.distance(), 4)
 
 
-def test_model_solves_instance_with_multiple_trips():
+def test_model_solves_single_route_instance_with_multiple_trips():
     """
     Smoke test to check that the model can solve an instance with multiple
-    trips.
+    trips for a single route.
     """
     m = Model()
     m.add_depot(x=1, y=1)
@@ -964,6 +964,49 @@ def test_model_solves_instance_with_multiple_trips():
     assert_equal(route.excess_load(), [0])
     assert_equal(route.num_trips(), 3)
     assert_equal(route.delivery(), [15])
+
+
+def test_model_solves_instance_with_multiple_trips():
+    """
+    Smoke test to check that the model can solve an instance with multiple
+    trips.
+    """
+    m = Model()
+    depot0 = m.add_depot(x=1, y=1)
+    depot1 = m.add_depot(x=3, y=3)
+    m.add_client(x=1, y=2, delivery=[5])
+    m.add_client(x=2, y=1, delivery=[5])
+    m.add_client(x=2, y=2, delivery=[5])
+    m.add_client(x=3, y=2, delivery=[5])
+
+    for frm in m.locations:
+        for to in m.locations:
+            manhattan = abs(frm.x - to.x) + abs(frm.y - to.y)
+            m.add_edge(frm, to, distance=manhattan)
+
+    m.add_vehicle_type(
+        1, capacity=[5], max_trips=2, start_depot=depot0, end_depot=depot0
+    )
+    m.add_vehicle_type(
+        1, capacity=[10], max_trips=1, start_depot=depot1, end_depot=depot1
+    )
+
+    res = m.solve(stop=MaxIterations(10))
+
+    assert_(res.is_feasible())
+    assert_equal(res.best.num_routes(), 2)
+
+    route1, route2 = res.best.routes()
+    assert_equal(route1.num_trips(), 2)
+    assert_equal(route1.excess_load(), [0])
+    assert_equal(route1.visits(), [2, 3])
+    assert_equal(route1.trip(0), [2])
+    assert_equal(route1.trip(1), [3])
+
+    assert_equal(route2.num_trips(), 1)
+    assert_equal(route2.excess_load(), [0])
+    assert_equal(route2.visits(), [4, 5])
+    assert_equal(route2.trip(0), [4, 5])
 
 
 def test_bug_client_group_indices():
