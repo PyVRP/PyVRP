f6011d90d3775f7338596d7be7c801585e715f66
Leon Lan <l.lan@vu.nl>
2024-12-29 08:50:05 +0100

    Revert LS

--- pyvrp/cpp/search/LocalSearch.cpp
+++ pyvrp/cpp/search/LocalSearch.cpp
@@ -1,4 +1,5 @@
 #include "LocalSearch.h"
+#include "DynamicBitset.h"
 #include "Measure.h"
 #include "primitives.h"
 
@@ -11,18 +12,18 @@ using pyvrp::search::LocalSearch;
 
 Solution LocalSearch::operator()(Solution const &solution,
                                  CostEvaluator const &costEvaluator,
+                                 std::vector<size_t> const &candidateNodes,
                                  double overlapTolerance)
 {
+    numEvaluations = 0;
     loadSolution(solution);
 
-    while (true)
-    {
-        search(costEvaluator);
-        intensify(costEvaluator, overlapTolerance);
+    candidates.reset();
+    for (auto idx : candidateNodes)
+        candidates[idx] = true;
 
-        if (numMoves == 0)  // then the current solution is locally optimal.
-            break;
-    }
+    search(costEvaluator);
+    intensify(costEvaluator, overlapTolerance);
 
     return exportSolution();
 }
@@ -54,11 +55,16 @@ void LocalSearch::search(CostEvaluator const &costEvaluator)
     // last *actually* modified.
     std::vector<int> lastTestedNodes(data.numLocations(), -1);
     lastModified = std::vector<int>(data.numVehicles(), 0);
-
-    searchCompleted = false;
     numMoves = 0;
 
-    for (int step = 0; !searchCompleted; ++step)
+    for (auto const uClient : orderNodes)  // insert all customers first
+    {
+        auto *U = &nodes[uClient];
+        applyOptionalClientMoves(U, costEvaluator);
+    }
+
+    bool firstStep = true;
+    while (candidates.any() && !searchCompleted)
     {
         searchCompleted = true;
 
@@ -80,8 +86,14 @@ void LocalSearch::search(CostEvaluator const &costEvaluator)
             if (!U->route())  // we already evaluated inserting U, so there is
                 continue;     // nothing left to be done for this client.
 
-            // We next apply the regular node operators. These work on pairs
-            // of nodes (U, V), where both U and V are in the solution.
+            // We next evaluate node operators that work on pairs of nodes
+            // of nodes (U, V), where both U and V are in the solution. We
+            // only do this if U is a promising candidate for improvement.
+            if (!candidates[uClient])
+                continue;
+
+            candidates[uClient] = false;
+
             for (auto const vClient : neighbours_[uClient])
             {
                 auto *V = &nodes[vClient];
@@ -102,9 +114,11 @@ void LocalSearch::search(CostEvaluator const &costEvaluator)
 
             // Moves involving empty routes are not tested in the first
             // iteration to avoid using too many routes.
-            if (step > 0)
+            if (!firstStep)
                 applyEmptyRouteMoves(U, costEvaluator);
         }
+
+        firstStep = false;
     }
 }
 
@@ -171,6 +185,8 @@ bool LocalSearch::applyNodeOps(Route::Node *U,
     for (auto *nodeOp : nodeOps)
     {
         auto const deltaCost = nodeOp->evaluate(U, V, costEvaluator);
+        numEvaluations += 1;
+
         if (deltaCost < 0)
         {
             auto *rU = U->route();  // copy these because the operator can
@@ -182,6 +198,26 @@ bool LocalSearch::applyNodeOps(Route::Node *U,
 
             nodeOp->apply(U, V);
             update(rU, rV);
+
+            if (!U->isDepot())
+                candidates[U->client()] = true;
+
+            if (!p(U)->isDepot())
+                candidates[p(U)->client()] = true;
+
+            if (!n(U)->isDepot())
+                candidates[n(U)->client()] = true;
+
+            if (!V->isDepot())
+            {
+                candidates[V->client()] = true;
+
+                if (!p(V)->isDepot())
+                    candidates[p(V)->client()] = true;
+
+                if (!n(V)->isDepot())
+                    candidates[n(V)->client()] = true;
+            }
 
             [[maybe_unused]] auto const costAfter
                 = costEvaluator.penalisedCost(*rU)
@@ -473,6 +509,7 @@ LocalSearch::LocalSearch(ProblemData const &data, Neighbours neighbours)
       neighbours_(data.numLocations()),
       orderNodes(data.numClients()),
       orderRoutes(data.numVehicles()),
+      candidates(data.numLocations()),
       lastModified(data.numVehicles(), -1)
 {
     setNeighbours(neighbours);
