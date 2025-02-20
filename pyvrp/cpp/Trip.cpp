9550c46ff2a0a42de38587176ce23f087dcc95c2
Niels Wouda <nielswouda@gmail.com>
2025-02-20 21:00:46 +0100

    Remove superfluous attribute

--- pyvrp/cpp/Trip.cpp
+++ pyvrp/cpp/Trip.cpp
@@ -2,11 +2,12 @@
 
 using pyvrp::Trip;
 
-Trip::Trip([[maybe_unused]] [[maybe_unused]] ProblemData const &data,
+Trip::Trip([[maybe_unused]] ProblemData const &data,
            [[maybe_unused]] Visits visits,
            TripDelimiter start,
            TripDelimiter end,
            [[maybe_unused]] Trip const *after)
     : start_(std::move(start)), end_(std::move(end))
 {
+    // TODO
 }
