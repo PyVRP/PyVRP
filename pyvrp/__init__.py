a683baafa25f45a305fea3571ab078e742b44cb7
Niels Wouda <nielswouda@gmail.com>
2025-02-20 11:59:48 +0100

    Add Trip to top-level module file, test stub

--- pyvrp/__init__.py
+++ pyvrp/__init__.py
@@ -19,6 +19,7 @@ from ._pyvrp import RandomNumberGenerator as RandomNumberGenerator
 from ._pyvrp import Reload as Reload
 from ._pyvrp import Route as Route
 from ._pyvrp import Solution as Solution
+from ._pyvrp import Trip as Trip
 from ._pyvrp import VehicleType as VehicleType
 from .minimise_fleet import minimise_fleet as minimise_fleet
 from .read import read as read
