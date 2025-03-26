c779a1fbaf47fef20930baf4266c7a915fac6882
Leon Lan <l.lan@vu.nl>
2025-03-26 17:06:29 +0100

    Include all ops

--- pyvrp/search/__init__.py
+++ pyvrp/search/__init__.py
@@ -23,17 +23,17 @@ from .neighbourhood import compute_neighbours as compute_neighbours
 NODE_OPERATORS: list[Type[NodeOperator]] = [
     Exchange10,
     Exchange20,
-    # Exchange30,
+    Exchange30,
     Exchange11,
-    # Exchange21,
-    # Exchange31,
-    # Exchange22,
-    # Exchange32,
-    # Exchange33,
+    Exchange21,
+    Exchange31,
+    Exchange22,
+    Exchange32,
+    Exchange33,
     SwapTails,
 ]
 
 ROUTE_OPERATORS: list[Type[RouteOperator]] = [
     # SwapRoutes,
-    # SwapStar,
+    SwapStar,
 ]
