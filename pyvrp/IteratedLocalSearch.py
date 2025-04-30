ec5ec3790e4dee2b883c9b5444bf183772faf348
Leon Lan <l.lan@vu.nl>
2025-04-30 06:33:56 +0200

    Revert "Increase num_destroy to 25"
    This reverts commit 91292ab2e8564de818c6247adf8d618b94e0d7c7.

--- pyvrp/IteratedLocalSearch.py
+++ pyvrp/IteratedLocalSearch.py
@@ -129,7 +129,7 @@ class IteratedLocalSearch:
             perturbed = self._perturb(
                 current,
                 self._pm.booster_cost_evaluator(),
-                25,  # hardcoded value
+                20,  # hardcoded value
             )
             diff = different_neighbours(current, perturbed)
             candidate = self._search(
