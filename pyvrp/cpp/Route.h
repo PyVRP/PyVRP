fcd06d383453e1d6e67666b2e1235449539e316d
Niels Wouda <N-Wouda@users.noreply.github.com>
2025-05-03 13:23:58 +0200

    Remove depot service duration (#776)
    * Remove depot service duration
    
    * Add to FAQ

--- pyvrp/cpp/Route.h
+++ pyvrp/cpp/Route.h
@@ -152,7 +152,7 @@ public:
     [[nodiscard]] Cost durationCost() const;
 
     /**
-     * Total duration of service on this route, at depots and clients.
+     * Total duration of client service on this route.
      */
     [[nodiscard]] Duration serviceDuration() const;
 
