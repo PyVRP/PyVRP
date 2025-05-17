be1a7dc9692014344d81d7689b1be54185e1d0d6
Leon Lan <l.lan@vu.nl>
2025-05-17 08:24:24 +0200

    Destroy in [10-30]

--- pyvrp/IteratedLocalSearch.py
+++ pyvrp/IteratedLocalSearch.py
@@ -129,7 +129,7 @@ class IteratedLocalSearch:
             perturbed = self._perturb(
                 current,
                 self._pm.booster_cost_evaluator(),
-                self._conv_manager.num_destroy,
+                10 + self._rng.randint(20),
             )
             diff = different_neighbours(current, perturbed)
             candidate = self._search(
