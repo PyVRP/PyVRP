5d7933dcc2a00dcf088b38218d35c2779578bab8
Leon Lan <l.lan@vu.nl>
2025-05-27 08:49:26 +0200

    Add iterations convergence to MAT

--- pyvrp/accept/MovingAverageThreshold.py
+++ pyvrp/accept/MovingAverageThreshold.py
@@ -12,27 +12,29 @@ class MovingAverageThreshold:
 
     .. math::
 
-       f(s^*) + \\eta(t) \\times \\left(
-          \\sum_{i = 1}^\\gamma \\frac{f(s^i)}{\\gamma} - f(s^*)
+       f(s^*) + \\tilde{\\eta} \\times \\left(
+          \\sum_{j = 1}^\\gamma \\frac{f(s^j)}{\\gamma} - f(s^*)
        \\right)
 
     where :math:`s^*` is the best solution observed in the last :math:`\\gamma`
     iterations, :math:`f(\\cdot)` indicates the objective function,
-    :math:`\\gamma \\in \\mathbb{N}` is a parameter, and each :math:`s^i` is a
+    :math:`\\gamma \\in \\mathbb{N}` is a parameter, and each :math:`s^j` is a
     recently observed solution. The recently observed solutions are stored in
     a ``history`` attributed of size at most :math:`\\gamma`.
 
-    The algorithm incorporates a time-dependent :math:`\\eta(t)` parameter that
-    converges to zero as the search approaches its time limit. It is calculated
-    as:
+    The algorithm incorporates a decreasing :math:`\\tilde{\\eta}` factor that
+    converges to zero as the search approaches its maximum time limit or
+    iterations. It is calculated as:
 
     .. math::
-        \\eta(t) = \\eta \\times \\left(1 -
-            \\frac{elapsed\\_time}{max\\_runtime}\\right)
+        \\tilde{\\eta} = \\eta \\times \\min\\left( 1 - \\frac{t}{T},
+            1 - \\frac{i}{I} \\right)
 
-    where :math:`\\eta \\ge 0` is a parameter, :math:`elapsed\\_time` is the
-    time elapsed since the start of the algorithm, and :math:`max\\_runtime` is
-    the maximum runtime of the algorithm.
+    where :math:`\\eta \\ge 0` is a parameter, :math:`t` is the time elapsed
+    since the start of the algorithm, :math:`T` is the imposed maximum runtime,
+    :math:`i` is the number of iterations performed, and :math:`I` is the
+    maximum number of iterations allowed. The most restrictive limit between
+    the two is used to compute the factor.
 
     Parameters
     ----------
@@ -43,6 +45,10 @@ class MovingAverageThreshold:
         History size. Must be positive.
     max_runtime
         Maximum runtime in seconds. As the search approaches this time limit,
+        :math:`\\eta` converges to zero. Must be non-negative. Default is
+        ``float("inf")``, meaning that :math:`\\eta` stays constant.
+    max_iterations
+        Maximum number of iterations. As the search approaches this limit,
         :math:`\\eta` converges to zero. Must be non-negative. Default is
         ``float("inf")``, meaning that :math:`\\eta` stays constant.
 
@@ -59,6 +65,7 @@ class MovingAverageThreshold:
         eta: float,
         gamma: int,
         max_runtime: float = float("inf"),
+        max_iterations: float = float("inf"),
     ):
         if eta < 0:
             raise ValueError("eta must be non-negative.")
@@ -69,13 +76,17 @@ class MovingAverageThreshold:
         if max_runtime < 0:
             raise ValueError("max_runtime must be non-negative.")
 
+        if max_iterations < 0:
+            raise ValueError("max_iterations must be non-negative.")
+
         self._eta = eta
         self._gamma = gamma
         self._max_runtime = max_runtime
+        self._max_iterations = max_iterations
 
         self._history = np.zeros(gamma)
         self._start_time = perf_counter()
-        self._idx = 0
+        self._iters = 0
 
     @property
     def eta(self) -> float:
@@ -89,20 +100,28 @@ class MovingAverageThreshold:
     def max_runtime(self) -> float:
         return self._max_runtime
 
+    @property
+    def max_iterations(self) -> float:
+        return self._max_iterations
+
     def __call__(self, best: float, current: float, candidate: float) -> bool:
-        idx = self._idx % self.gamma
+        idx = self._iters % self.gamma
         self._history[idx] = candidate
-        self._idx += 1
 
         history = self._history
-        if self._idx < self.gamma:  # not yet enough solutions observed
-            history = history[: self._idx]
+        if self._iters < self.gamma:  # not yet enough solutions observed
+            history = history[: self._iters + 1]
 
         recent_best = history.min()
         recent_avg = history.mean()
 
-        runtime = min(perf_counter() - self._start_time, self._max_runtime)
-        pct_elapsed = runtime / self.max_runtime if self.max_runtime else 1
-        factor = self.eta * (1 - pct_elapsed)
+        runtime = min(perf_counter() - self._start_time, self.max_runtime)
+        pct_runtime = runtime / self.max_runtime if self.max_runtime else 1
+        pct_iters = (
+            self._iters / self.max_iterations if self.max_iterations else 1
+        )
+        factor = self.eta * min(1 - pct_runtime, 1 - pct_iters)
+
+        self._iters += 1
 
         return candidate <= recent_best + factor * (recent_avg - recent_best)
--- tests/accept/test_MovingAverageThreshold.py
+++ tests/accept/test_MovingAverageThreshold.py
@@ -5,40 +5,57 @@ from pyvrp.accept import MovingAverageThreshold
 
 
 @mark.parametrize(
-    "eta, gamma, max_runtime",
+    "eta, gamma, max_runtime, max_iterations",
     [
-        (-1, 1, 0),  # eta cannot be < 0
-        (1, -2, 0),  # gamma cannot be < 0
-        (1, 0, 0),  # gamma cannot be 0
-        (1, 1, -1),  # max_runtime cannot be < 0
+        (-1, 1, 0, 0),  # eta cannot be < 0
+        (1, -2, 0, 0),  # gamma cannot be < 0
+        (1, 0, 0, 0),  # gamma cannot be 0
+        (1, 1, -1, 0),  # max_runtime cannot be < 0
+        (1, 1, 0, -1),  # max_iterations cannot be < 0
     ],
 )
-def test_raise_invalid_parameters(eta: float, gamma: int, max_runtime: float):
+def test_raise_invalid_parameters(
+    eta: float, gamma: int, max_runtime: float, max_iterations: int
+):
     """
     Tests that a ValueError is raised when invalid parameters are passed.
     """
     with assert_raises(ValueError):
-        MovingAverageThreshold(eta=eta, gamma=gamma, max_runtime=max_runtime)
+        MovingAverageThreshold(
+            eta=eta,
+            gamma=gamma,
+            max_runtime=max_runtime,
+            max_iterations=max_iterations,
+        )
 
 
 @mark.parametrize(
-    "eta, gamma, max_runtime",
+    "eta, gamma, max_runtime, max_iterations",
     [
-        (1, 3, 0),
-        (0.4, 4, 10),
+        (1, 2, 3, 4),
+        (0.4, 4, 10.1, 100),
     ],
 )
 def test_attributes_are_correctly_set(
-    eta: float, gamma: int, max_runtime: int
+    eta: float,
+    gamma: int,
+    max_runtime: float,
+    max_iterations: float,
 ):
     """
     Tests that valid parameters are correctly set.
     """
-    mat = MovingAverageThreshold(eta=eta, gamma=gamma, max_runtime=max_runtime)
+    mat = MovingAverageThreshold(
+        eta=eta,
+        gamma=gamma,
+        max_runtime=max_runtime,
+        max_iterations=max_iterations,
+    )
 
     assert_equal(mat.eta, eta)
     assert_equal(mat.gamma, gamma)
     assert_equal(mat.max_runtime, max_runtime)
+    assert_equal(mat.max_iterations, max_iterations)
 
 
 def test_accepts_below_threshold():
@@ -97,15 +114,62 @@ def test_threshold_updates_with_history():
     assert_(not mat(1, 1, 2))
 
 
+def test_threshold_is_always_recent_best_with_gamma_one():
+    """
+    Tests that MAT always accepts the candidate solution if gamma is 1.
+    """
+    mat = MovingAverageThreshold(eta=0.5, gamma=1)
+
+    # With gamma=1, the history always contains the candidate solution
+    # and is of size 1, so the threshold is always equal to the candidate.
+    assert_(mat(1, 1, 1))
+    assert_(mat(1, 1, 10))
+    assert_(mat(1, 1, 100))
+    assert_(mat(1, 1, 1000))
+
+
 def test_threshold_converges_with_max_runtime():
     """
     Tests that MAT correctly converges the threshold based on the runtime.
     """
-    mat = MovingAverageThreshold(eta=0.5, gamma=10, max_runtime=0)
+    mat = MovingAverageThreshold(eta=1, gamma=10, max_runtime=0)
 
     mat(1, 1, 10)
     mat(1, 1, 0)
 
     # Even though the candidate solutions are (10, 0, 2), the threshold
-    # is set to 0 because the max runtime has elapsed.
+    # is set to 0 because the maximum runtime has been reached.
     assert_(not mat(1, 1, 2))
+
+
+def test_threshold_converges_with_max_iterations():
+    """
+    Tests that MAT correctly converges the threshold based on the runtime.
+    """
+    mat = MovingAverageThreshold(eta=1, gamma=10, max_iterations=2)
+
+    # Threshold is set at 2 + 1 * (2 - 2) = 2, so accept.
+    assert_(mat(1, 1, 2))
+
+    # Threshold is set at 1 + 0.5 * (1.5 - 1) = 1.25, so accept.
+    assert_(mat(1, 1, 1))
+
+    # The threshold is set at 1 + 0.0 * (1.5 - 1) = 1, so reject.
+    assert_(not mat(1, 1, 1.5))
+
+
+def test_threshold_converges_with_most_restricting_limit():
+    """
+    Tests that MAT correctly converges the threshold based on the most
+    restrictive condition.
+    """
+    mat = MovingAverageThreshold(
+        eta=1, gamma=10, max_runtime=100, max_iterations=2
+    )
+
+    mat(1, 1, 2)
+    mat(1, 1, 1)
+
+    # Maximum iterations has been reached, so the factor is set to 0,
+    # even though the maximum runtime hasn't elapsed yet.
+    assert_(not mat(1, 1, 1.5))
