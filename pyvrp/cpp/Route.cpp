68a696c89aa6256113a5c1d32d6d61a5aca7fbf9
Niels Wouda <nielswouda@gmail.com>
2025-09-15 22:17:47 +0200

    Simplify Route::schedule a bit

--- pyvrp/cpp/Route.cpp
+++ pyvrp/cpp/Route.cpp
@@ -136,8 +136,6 @@ void Route::makeSchedule(ProblemData const &data)
     auto const &vehData = data.vehicleType(vehicleType_);
     auto const &durations = data.durationMatrix(vehData.profile);
 
-    // TODO rethink the following
-
     auto now = startTime_;
     auto const handle
         = [&](auto const &where, size_t location, size_t trip, Duration service)
@@ -158,15 +156,16 @@ void Route::makeSchedule(ProblemData const &data)
         auto const &trip = trips_[tripIdx];
         ProblemData::Depot const &start = data.location(trip.startDepot());
 
-        DurationSegment ds
-            = {0, 0, start.twEarly, start.twLate, trip.releaseTime()};
-        if (tripIdx == 0)
-            ds = DurationSegment::merge(0, {vehData, vehData.startLate}, ds);
+        auto const earliestStart = std::max(
+            start.twEarly, std::min(trip.releaseTime(), start.twLate));
+        auto const wait = std::max<Duration>(earliestStart - now, 0);
 
-        auto const wait = std::max<Duration>(ds.startEarly() - now, 0);
-        auto const tw = tripIdx == 0
-                            ? ds.timeWarp()
-                            : std::max<Duration>(now - ds.startLate(), 0);
+        auto tw = std::max<Duration>(now - start.twLate, 0);
+        if (tripIdx == 0)  // then we also need to account for the vehicle's
+        {                  // latest start constraint, if binding.
+            auto const latestStart = std::min(start.twLate, vehData.startLate);
+            tw = std::max<Duration>(now - latestStart, 0);
+        }
 
         now += wait;
         now -= tw;
