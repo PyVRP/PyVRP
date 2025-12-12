5969372033296a910f8e6bb601084d375f1d9e3e
Niels Wouda <nielswouda@gmail.com>
2025-12-12 13:56:30 +0100

    Revert a bit

--- pyvrp/search/LocalSearch.py
+++ pyvrp/search/LocalSearch.py
@@ -41,7 +41,7 @@ class LocalSearch:
     ):
         self._ls = _LocalSearch(data, neighbours)
         self._rng = rng
-        self._num_perturbations = max(num_perturbations, 1)
+        self._num_perturbations = num_perturbations
 
     def add_node_operator(self, op: NodeOperator):
         """
@@ -129,8 +129,8 @@ class LocalSearch:
             The improved solution. This is not the same object as the
             solution that was passed in.
         """
-        num_perturbations = self._rng.randint(self._num_perturbations) + 1
-        self._ls.num_perturbations = num_perturbations
+        num_perturbations = max(self._num_perturbations, 1)
+        self._ls.num_perturbations = self._rng.randint(num_perturbations) + 1
 
         self._ls.shuffle(self._rng)
         return self._ls(solution, cost_evaluator)
