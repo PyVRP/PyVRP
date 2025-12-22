24ab630ef67096a676bfb3385671522ac5401114
Niels Wouda <nielswouda@gmail.com>
2025-12-22 11:55:17 +0100

    Remove crossover, diversity, and population

--- pyvrp/__init__.py
+++ pyvrp/__init__.py
@@ -10,8 +10,6 @@ from .Model import Model as Model
 from .Model import Profile as Profile
 from .PenaltyManager import PenaltyManager as PenaltyManager
 from .PenaltyManager import PenaltyParams as PenaltyParams
-from .Population import Population as Population
-from .Population import PopulationParams as PopulationParams
 from .Result import Result as Result
 from .Statistics import Statistics as Statistics
 from ._pyvrp import Client as Client
--- pyvrp/crossover/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ordered_crossover import ordered_crossover as ordered_crossover
-from .selective_route_exchange import (
-    selective_route_exchange as selective_route_exchange,
-)
