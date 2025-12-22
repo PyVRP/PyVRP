c0d9b692898ead1748d3f54443607d4d314fd87d
Leon Lan <l.lan@vu.nl>
2025-12-22 15:43:23 +0100

    Tighten `plot_objectives` y-lim defaults (#972)

--- pyvrp/plotting/plot_objectives.py
+++ pyvrp/plotting/plot_objectives.py
@@ -8,7 +8,7 @@ def plot_objectives(
     result: Result,
     num_to_skip: int | None = None,
     ax: plt.Axes | None = None,
-    ylim_adjust: tuple[float, float] = (0.95, 1.15),
+    ylim_adjust: tuple[float, float] = (0.99, 1.05),
 ):
     """
     Plots each iteration's objective values.
@@ -24,9 +24,9 @@ def plot_objectives(
     ax
         Axes object to draw the plot on. One will be created if not provided.
     ylim_adjust
-        Bounds the y-axis to ``(best * ylim_adjust[0], best * ylim_adjust[1])``
-        where ``best`` denotes the best found feasible objective value. Default
-        (0.95, 1.15).
+        Optional adjustment to bound the y-axis to ``(best * ylim_adjust[0],
+        best * ylim_adjust[1])`` where ``best`` denotes the best found feasible
+        objective value.
     """
     if not ax:
         _, ax = plt.subplots()
