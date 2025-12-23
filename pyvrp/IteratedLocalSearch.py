d39d17ecb03dc4896642dbfabaa115d38f95d276
Leon Lan <l.lan@vu.nl>
2025-12-23 17:45:09 +0100

    Higher decay rate

--- pyvrp/IteratedLocalSearch.py
+++ pyvrp/IteratedLocalSearch.py
@@ -43,7 +43,7 @@ class IteratedLocalSearchParams:
     initial_accept_weight: float = 1
     history_length: int = 500
     budget: int = 20_000
-    decay_rate: float = 0.5
+    decay_rate: float = 0.75
 
     def __post_init__(self):
         if self.num_iters_no_improvement < 0:
@@ -145,9 +145,11 @@ class IteratedLocalSearch:
             if iters_no_improvement == self._params.num_iters_no_improvement:
                 print_progress.restart()
                 history.clear()
+                history.append(cost_eval.penalised_cost(best))
 
                 current = best
                 iters_no_improvement = 0
+                base_weight = self._params.initial_accept_weight
 
             cost_eval = self._pm.cost_evaluator()
             candidate = self._search(current, cost_eval)
@@ -173,7 +175,7 @@ class IteratedLocalSearch:
             if cand_cost <= best_weight + mean_weight:
                 current = candidate
 
-            if iters_budget >= self._params.budget:
+            if iters_budget == self._params.budget:
                 history.clear()
                 history.append(cost_eval.penalised_cost(best))
                 current = best
