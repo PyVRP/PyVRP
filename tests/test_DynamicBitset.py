32ebd4246d887be92c6df6c1f708864aa9fd441e
Leon Lan <l.lan@vu.nl>
2026-02-11 17:58:03 +0100

    Revert test changes

--- tests/test_DynamicBitset.py
+++ tests/test_DynamicBitset.py
@@ -31,7 +31,7 @@ def test_eq():
     """
     bitset1 = DynamicBitset(64)
     bitset2 = DynamicBitset(64)
-    assert_equal(bitset1, bitset2)  # both empty, equal size
+    assert_(bitset1 == bitset2)  # both empty, equal size - should be the same
 
     bitset2[0] = True
     assert_(bitset1 != bitset2)  # 2 is no longer empty; should not be the same
