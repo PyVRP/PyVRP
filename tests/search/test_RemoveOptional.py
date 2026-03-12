659dbace847f902eef06b3f23359614a06bddfcb
Niels Wouda <nielswouda@gmail.com>
2026-03-12 13:26:47 +0100

    Fix RemoveOptional tests

--- tests/search/test_RemoveOptional.py
+++ tests/search/test_RemoveOptional.py
@@ -35,7 +35,7 @@ def test_does_not_remove_required_clients():
         duration_matrices=[np.zeros((3, 3), dtype=int)],
     )
 
-    route = make_search_route(data, [1, 2])
+    route = make_search_route(data, ["C0", "C1"])
     cost_eval = CostEvaluator([100], 100, 0)
     op = RemoveOptional(data)
 
@@ -46,7 +46,7 @@ def test_does_not_remove_required_clients():
 
     # Should remove the second client.
     op.apply(route[2])
-    assert_equal(str(route), "1")
+    assert_equal(str(route), "C0")
 
 
 def test_supports(
@@ -68,7 +68,7 @@ def test_supports(
         vehicle_types=[VehicleType()],
         distance_matrices=[np.zeros((1, 1), dtype=int)],
         duration_matrices=[np.zeros((1, 1), dtype=int)],
-        groups=[ClientGroup(clients=[1], required=False)],
+        groups=[ClientGroup(clients=[0], required=False)],
     )
 
     # RemoveOptional does not support instances with required groups, but it
@@ -101,12 +101,12 @@ def test_fixed_vehicle_cost():
     # can happen due to vehicle changes. In this, case we evaluate removing the
     # only client on a route. That makes the route empty, and removes the fixed
     # vehicle cost of 7 for this vehicle type.
-    route = make_search_route(data, [1], vehicle_type=0)
+    route = make_search_route(data, ["C0"], vehicle_type=0)
     assert_equal(op.evaluate(route[1], cost_eval), (-7, True))
 
     # Same story for this route, but now we have a different vehicle type with
     # fixed cost 13.
-    route = make_search_route(data, [1], vehicle_type=1)
+    route = make_search_route(data, ["C0"], vehicle_type=1)
     assert_equal(op.evaluate(route[1], cost_eval), (-13, True))
 
 
@@ -115,13 +115,13 @@ def test_remove(ok_small_prizes):
     Tests that RemoveOptional works correctly on a specific example.
     """
     cost_eval = CostEvaluator([1], 1, 0)
-    route = make_search_route(ok_small_prizes, [1, 2])
+    route = make_search_route(ok_small_prizes, ["C0", "C1"])
 
     op = RemoveOptional(ok_small_prizes)
 
-    # Purely distance. Removes arcs 1 -> 2 -> 0, adds arcs 1 -> 0. This change
-    # has delta distance of 1726 - 1992 - 1965 = -2231, and a prize delta of
-    # 15: -2231 + 15 = -2216.
+    # Purely distance. Removes arcs C1 -> C2 -> D0, adds arcs C1 -> 0. This has
+    # delta distance of 1726 - 1992 - 1965 = -2231, and a prize delta of 15:
+    # -2231 + 15 = -2216.
     assert_equal(op.evaluate(route[2], cost_eval), (-2216, True))
 
 
@@ -155,7 +155,7 @@ def test_empty_route_delta_cost_bug():
 
     # Similarly, if removing a client results in an empty route, then we should
     # not include the empty route's costs.
-    route = make_search_route(data, [2])
+    route = make_search_route(data, ["C0"])
     assert_equal(op.evaluate(route[1], cost_eval), (-4, True))
 
 
@@ -175,8 +175,8 @@ def test_cannot_remove_required_group():
         duration_matrices=[np.where(np.eye(3), 0, 1)],
         distance_matrices=[np.where(np.eye(3), 0, 1)],
         groups=[
-            ClientGroup([1], required=False),
-            ClientGroup([2], required=True),
+            ClientGroup([0], required=False),
+            ClientGroup([1], required=True),
         ],
     )
 
@@ -185,7 +185,7 @@ def test_cannot_remove_required_group():
 
     op = RemoveOptional(data)
     cost_eval = CostEvaluator([], 1, 1)
-    route = make_search_route(data, [1, 2])
+    route = make_search_route(data, ["C0", "C1"])
 
     # The first client in the route is from the optional group and can be
     # removed. The second client is from the required group, and cannot.
