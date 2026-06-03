247ea7b77ed3dce847048593b7a7a8c64f96b429
Niels Wouda <nielswouda@gmail.com>
2026-06-03 09:27:35 +0200

    Add data members

--- pyvrp/cpp/Shipment.cpp
+++ pyvrp/cpp/Shipment.cpp
@@ -1,3 +1,24 @@
 #include "Shipment.h"
 
-// TODO
+#include <stdexcept>
+
+using pyvrp::Shipment;
+
+Shipment::Step::Step(size_t location,
+                     Duration twEarly,
+                     Duration twLate,
+                     Duration serviceDuration)
+    : location(location),
+      twEarly(twEarly),
+      twLate(twLate),
+      serviceDuration(serviceDuration)
+{
+    if (serviceDuration < 0)
+        throw std::invalid_argument("service_duration must be >= 0.");
+
+    if (twEarly > twLate)
+        throw std::invalid_argument("tw_early must be <= tw_late.");
+
+    if (twEarly < 0)
+        throw std::invalid_argument("tw_early must be >= 0.");
+}
--- pyvrp/cpp/Shipment.h
+++ pyvrp/cpp/Shipment.h
@@ -1,14 +1,49 @@
 #ifndef PYVRP_SHIPMENT_H
 #define PYVRP_SHIPMENT_H
 
+#include "Measure.h"
+
+#include <limits>
+
 namespace pyvrp
 {
 /**
  * TODO
  */
-class Shipment
+struct Shipment
 {
-    // TODO
+    /**
+     * Shared data for a pickup or delivery step.
+     */
+    struct Step
+    {
+        size_t const location;
+        Duration const twEarly;
+        Duration const twLate;
+        Duration const serviceDuration;
+
+        Step(size_t location,
+             Duration twEarly = 0,
+             Duration twLate = std::numeric_limits<Duration>::max(),
+             Duration serviceDuration = 0);
+
+        Step(Step const &step) = default;
+        Step(Step &&step) = default;
+
+        bool operator==(Step const &other) const = default;
+
+        Step &operator=(Step const &step) = delete;
+        Step &operator=(Step &&step) = delete;
+    };
+
+    Step const pickup;
+    Step const delivery;
+    std::vector<Load> const amount;
+    Cost const prize;
+    bool const required;
+    char const *name;
+
+    // TODO constructors
 };
 }  // namespace pyvrp
 
