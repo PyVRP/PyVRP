9e7c72ebce711d4669e6673e449af66262335417
Niels Wouda <nielswouda@gmail.com>
2026-06-17 16:09:04 +0200

    Implement ReplaceOptionalShipment

--- pyvrp/cpp/search/ReplaceOptionalShipment.cpp
+++ pyvrp/cpp/search/ReplaceOptionalShipment.cpp
@@ -1,27 +1,94 @@
 #include "ReplaceOptionalShipment.h"
 
+#include "DeliverySegment.h"
+#include "PickupSegment.h"
+
 #include <cassert>
 
 using pyvrp::search::ReplaceOptionalShipment;
 
 std::pair<pyvrp::Cost, bool> ReplaceOptionalShipment::evaluate(
-    [[maybe_unused]] Route::Node *U,
-    [[maybe_unused]] Route::Node *V,
-    [[maybe_unused]] CostEvaluator const &costEvaluator)
+    Route::Node *U, Route::Node *V, CostEvaluator const &costEvaluator)
 {
     stats_.numEvaluations++;
 
-    // TODO
+    if (U->route() || !V->route() || !U->isShipment() || !V->isShipment())
+        return std::make_pair(0, false);
 
-    return std::make_pair(0, false);
+    assert(U->isShipment() && V->isShipment());
+    auto const &uShipment = data.shipment(U->idx());
+    auto const &vShipment = data.shipment(V->idx());
+
+    if (vShipment.required)
+        return std::make_pair(0, false);
+
+    Route::Node *vPickup, *vDelivery;
+    if (V->isPickup())
+    {
+        vPickup = V;
+        vDelivery = V + 1;
+    }
+    else
+    {
+        vDelivery = V;
+        vPickup = V - 1;
+    }
+
+    assert(vPickup->pos() < vDelivery->pos());
+    auto const *route = V->route();
+
+    Cost deltaCost = vShipment.prize - uShipment.prize;
+    if (n(vPickup) == vDelivery)
+        costEvaluator.deltaCost(
+            deltaCost,
+            Route::Proposal(route->before(vPickup->pos() - 1),
+                            PickupSegment(data, U->idx()),
+                            DeliverySegment(data, U->idx()),
+                            route->after(vDelivery->pos() + 1)));
+    else
+        costEvaluator.deltaCost(
+            deltaCost,
+            Route::Proposal(
+                route->before(vPickup->pos() - 1),
+                PickupSegment(data, U->idx()),
+                route->between(vPickup->pos() + 1, vDelivery->pos() - 1),
+                DeliverySegment(data, U->idx()),
+                route->after(vDelivery->pos() + 1)));
+
+    return std::make_pair(deltaCost, deltaCost < 0);
 }
 
-void ReplaceOptionalShipment::apply([[maybe_unused]] Route::Node *U,
-                                    [[maybe_unused]] Route::Node *V) const
+void ReplaceOptionalShipment::apply(Route::Node *U, Route::Node *V) const
 {
+    assert(U->isShipment() && V->isShipment() && V->route() && !U->route());
     stats_.numApplications++;
 
-    // TODO
+    Route::Node *uPickup, *uDelivery;
+    if (U->isPickup())
+    {
+        uPickup = U;
+        uDelivery = U + 1;
+    }
+    else
+    {
+        uDelivery = U;
+        uPickup = U - 1;
+    }
+
+    Route::Node *vPickup, *vDelivery;
+    if (V->isPickup())
+    {
+        vPickup = V;
+        vDelivery = V + 1;
+    }
+    else
+    {
+        vDelivery = V;
+        vPickup = V - 1;
+    }
+
+    Route::swap(vPickup, uPickup);
+    Route::swap(vDelivery, uDelivery);
 }
 
 template <>
--- tests/search/test_ReplaceOptionalShipment.py
+++ tests/search/test_ReplaceOptionalShipment.py
@@ -1,1 +1,29 @@
-# TODO
+from numpy.testing import assert_
+
+from pyvrp.search import ReplaceOptionalShipment
+
+
+def test_replace_adjacent(small_optional_shipments):
+    """
+    TODO
+    """
+    pass
+
+
+def test_replace():
+    """
+    TODO
+    """
+    pass
+
+
+def test_supports(ok_small, small_shipments, small_optional_shipments):
+    """
+    Tests supports().
+    """
+    # This instance only has clients, not shipments.
+    assert_(not ReplaceOptionalShipment.supports(ok_small))
+
+    # The operator only supports instances with *optional* shipments.
+    assert_(not ReplaceOptionalShipment.supports(small_shipments))
+    assert_(ReplaceOptionalShipment.supports(small_optional_shipments))
