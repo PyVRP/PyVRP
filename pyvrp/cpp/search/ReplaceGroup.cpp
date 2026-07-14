a1950107007fefd3b821aa5dc9bf11515b953880
Leon Lan <leon@appliedrouting.com>
2026-07-14 11:25:36 +0200

    Fix prize bug in ReplaceGroup (#1132)

--- pyvrp/cpp/search/ReplaceGroup.cpp
+++ pyvrp/cpp/search/ReplaceGroup.cpp
@@ -28,6 +28,7 @@ ReplaceGroup::evaluate(Route::Node *U, CostEvaluator const &costEvaluator)
             V_ = &solution_->nodes[client];
             auto *route = V_->route();
 
+            deltaCost = data.client(client).prize - uData.prize;
             costEvaluator.deltaCost(  // evaluate replacing V with U
                 deltaCost,
                 Route::Proposal(route->before(V_->pos() - 1),
