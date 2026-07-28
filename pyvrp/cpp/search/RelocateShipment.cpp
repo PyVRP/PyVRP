b7b597f34281288616303ba409e9876e0a9a36b1
Niels Wouda <N-Wouda@users.noreply.github.com>
2026-07-28 11:31:02 +0200

    Add RelocateShipment operator (#1143)
    * Add RelocateShipment operator
    
    * First-improvement RelocatePickup
    
    * Add tests

---
+++ pyvrp/cpp/search/RelocateShipment.cpp
@@ -0,0 +1,115 @@
+#include "RelocateShipment.h"
+
+#include <cassert>
+
+using pyvrp::search::RelocateShipment;
+
+std::pair<pyvrp::Cost, bool> RelocateShipment::evaluate(
+    Route::Node *U, Route::Node *V, CostEvaluator const &costEvaluator)
+{
+    assert(V->route());
+    stats_.numEvaluations++;
+
+    if (!U->isPickup() || !U->route())
+        return std::make_pair(0, false);
+
+    if (U->route() == V->route())
+        // It is not technically impossible to relocate within the same route,
+        // but evaluating every possible route configuration requires dozens of
+        // branches and proposals, which is prohibitive to fully list here.
+        return std::make_pair(0, false);
+
+    move_ = {};
+
+    auto const *uRoute = U->route();
+    auto const *vRoute = V->route();
+
+    auto const *uPickup = U;
+    auto const *uDelivery = U + 1;
+
+    Cost fixedCost = 0;
+    if (uRoute->numShipments() == 1 && uRoute->numClients() == 0)
+        // Then U's route is empty after we relocate U, and we lose the route's
+        // fixed vehicle cost.
+        fixedCost -= uRoute->fixedVehicleCost();
+
+    if (vRoute->empty())  // will become non-empty after inserting U.
+        fixedCost += vRoute->fixedVehicleCost();
+
+    Cost removeCost = 0;
+    if (n(uPickup) != uDelivery)  // exact when removing U's shipment so we
+    {                             // have the correct delta cost for this part
+        auto const uProposal = Route::Proposal(
+            uRoute->before(uPickup->pos() - 1),
+            uRoute->between(uPickup->pos() + 1, uDelivery->pos() - 1),
+            uRoute->after(uDelivery->pos() + 1));
+
+        costEvaluator.deltaCost<true>(removeCost, uProposal);
+    }
+    else
+    {
+        auto const uProposal
+            = Route::Proposal(uRoute->before(uPickup->pos() - 1),
+                              uRoute->after(uDelivery->pos() + 1));
+
+        costEvaluator.deltaCost<true>(removeCost, uProposal);
+    }
+
+    Cost deltaCost = removeCost + fixedCost;
+    costEvaluator.deltaCost(deltaCost,  // delivery directly after pickup
+                            Route::Proposal(vRoute->before(V->pos()),
+                                            uRoute->at(uPickup->pos()),
+                                            uRoute->at(uDelivery->pos()),
+                                            vRoute->after(V->pos() + 1)));
+
+    if (deltaCost < 0)
+    {
+        move_ = {V->pos() + 1};
+        return std::make_pair(deltaCost, true);
+    }
+
+    // Pickup after V, delivery later in the route.
+    for (auto const *node = n(V); !node->isDepot(); node = n(node))
+    {
+        Cost deltaCost = removeCost + fixedCost;
+        costEvaluator.deltaCost(
+            deltaCost,
+            Route::Proposal(vRoute->before(V->pos()),
+                            uRoute->at(uPickup->pos()),
+                            vRoute->between(V->pos() + 1, node->pos()),
+                            uRoute->at(uDelivery->pos()),
+                            vRoute->after(node->pos() + 1)));
+
+        if (deltaCost < 0)
+        {
+            move_ = {node->pos() + 1};  // after node
+            return std::make_pair(deltaCost, true);
+        }
+    }
+
+    return std::make_pair(0, false);
+}
+
+void RelocateShipment::apply(Route::Node *U, Route::Node *V) const
+{
+    assert(U->isPickup() && U->route() && V->route());
+    stats_.numApplications++;
+
+    auto *uRoute = U->route();
+    uRoute->remove((U + 1)->pos());  // remove delivery
+    uRoute->remove(U->pos());        // remove pickup
+
+    auto *vRoute = V->route();
+    assert(move_.pos > V->pos());
+    vRoute->insert(move_.pos, U + 1);  // insert delivery
+    vRoute->insert(V->pos() + 1, U);   // insert pickup
+}
+
+std::string RelocateShipment::name() const { return "RelocateShipment"; }
+
+bool RelocateShipment::supports(ProblemData const &data)
+{
+    // Evaluates relocating shipments between routes, so we need at least one
+    // shipment and more than one vehicle.
+    return data.numShipments() > 0 && data.numVehicles() > 1;
+}
---
+++ pyvrp/cpp/search/RelocateShipment.h
@@ -0,0 +1,39 @@
+#ifndef PYVRP_SEARCH_RELOCATESHIPMENT_H
+#define PYVRP_SEARCH_RELOCATESHIPMENT_H
+
+#include "LocalSearchOperator.h"
+
+namespace pyvrp::search
+{
+/**
+ * RelocateShipment(data: ProblemData)
+ *
+ * Evaluates relocating the shipment :math:`U` after :math:`V` in different
+ * routes. :math:`U` is relocated directly after :math:`V`, and :math:`U`'s
+ * delivery is inserted in the first improving place after :math:`U`.
+ */
+class RelocateShipment : public BinaryOperator
+{
+    using BinaryOperator::BinaryOperator;
+
+    struct Move
+    {
+        size_t pos = 0;
+    };
+
+    Move move_;
+
+public:
+    std::pair<Cost, bool> evaluate(Route::Node *U,
+                                   Route::Node *V,
+                                   CostEvaluator const &costEvaluator) override;
+
+    void apply(Route::Node *U, Route::Node *V) const override;
+
+    std::string name() const override;
+
+    static bool supports(ProblemData const &data);
+};
+}  // namespace pyvrp::search
+
+#endif  // PYVRP_SEARCH_RELOCATESHIPMENT_H
