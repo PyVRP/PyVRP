73d63670195c2002c7a5f841e314175600cc362e
Niels Wouda <nielswouda@gmail.com>
2026-08-12 21:26:51 +0200

    Remove what's not needed

--- pyvrp/cpp/search/Solution.cpp
+++ pyvrp/cpp/search/Solution.cpp
@@ -14,6 +14,7 @@ using pyvrp::Cost;
 using pyvrp::Distance;
 using pyvrp::DurationSegment;
 using pyvrp::LoadSegment;
+using pyvrp::ProblemData;
 
 using pyvrp::search::Route;
 using pyvrp::search::SegmentProxy;
@@ -21,38 +22,42 @@ using pyvrp::search::Solution;
 
 namespace
 {
-class SegmentAccumulator
+class IncrementalSegmentBetween
 {
-    pyvrp::ProblemData const &data_;
-    Route const &route_;
-    SegmentProxy front_;
-    SegmentProxy back_;
-    size_t size_;
-    size_t numClients_;
-    size_t numPickups_;
-    Distance distance_;
+    ProblemData const &data_;
+    Route const *route_;
+    size_t start;
+    size_t end;
     DurationSegment duration_;
     std::vector<LoadSegment> loads_;
 
 public:
-    Route const *route() const { return &route_; }
-    SegmentProxy front() const { return front_; }
-    SegmentProxy back() const { return back_; }
-    size_t size() const { return size_; }
-    size_t numClients() const { return numClients_; }
-    size_t numPickups() const { return numPickups_; }
+    Route const *route() const { return route_; }
+
+    SegmentProxy front() const { return route_->at(start).front(); }
+    SegmentProxy back() const { return route_->at(end).back(); }
+
+    size_t size() const { return route_->between(start, end).size(); }
+    size_t numClients() const
+    {
+        return route_->between(start, end).numClients();
+    }
+    size_t numPickups() const
+    {
+        return route_->between(start, end).numPickups();
+    }
+
     bool startsAtReloadDepot() const { return false; }
     bool endsAtReloadDepot() const { return false; }
 
     Distance distance([[maybe_unused]] size_t profile) const
     {
-        assert(profile == route_.profile());
-        return distance_;
+        return route_->between(start, end).distance(profile);
     }
 
     DurationSegment duration([[maybe_unused]] size_t profile) const
     {
-        assert(profile == route_.profile());
+        assert(profile == route_->profile());
         return duration_;
     }
 
@@ -61,54 +66,37 @@ public:
         return loads_[dimension];
     }
 
-    template <pyvrp::search::Segment T>
-    SegmentAccumulator(pyvrp::ProblemData const &data,
-                       Route const &route,
-                       T const &segment)
+    IncrementalSegmentBetween(ProblemData const &data,
+                              Route const *route,
+                              size_t pos)
         : data_(data),
           route_(route),
-          front_(segment.front()),
-          back_(segment.back()),
-          size_(segment.size()),
-          numClients_(segment.numClients()),
-          numPickups_(segment.numPickups()),
-          distance_(segment.distance(route.profile())),
-          duration_(segment.duration(route.profile())),
-          loads_(route.capacity().size())
+          start(pos),
+          end(pos),
+          duration_(route_->at(pos).duration(route->profile())),
+          loads_(route_->capacity().size())
     {
-        assert(segment.route() == &route);
-        assert(!segment.startsAtReloadDepot());
-        assert(!segment.endsAtReloadDepot());
-
-        for (size_t dim = 0; dim != route.capacity().size(); ++dim)
-            loads_[dim] = segment.load(dim);
+        for (size_t dim = 0; dim != route_->capacity().size(); ++dim)
+            loads_[dim] = route_->at(pos).load(dim);
     }
 
-    template <pyvrp::search::Segment T> void merge(T const &segment)
+    void advance()
     {
-        assert(segment.route() == &route_);
-        assert(!segment.startsAtReloadDepot());
-        assert(!segment.endsAtReloadDepot());
+        auto const profile = route_->profile();
+        auto const &matrix = data_.durationMatrix(profile);
 
-        auto const profile = route_.profile();
-        auto const from = back_.location();
+        auto const segment = route_->at(end + 1);
+
+        auto const from = back().location();
         auto const to = segment.front().location();
 
-        distance_ += data_.distanceMatrix(profile)(from, to);
-        distance_ += segment.distance(profile);
-
-        duration_
-            = DurationSegment::merge(data_.durationMatrix(profile)(from, to),
-                                     duration_,
-                                     segment.duration(profile));
+        duration_ = DurationSegment::merge(
+            matrix(from, to), duration_, segment.duration(profile));
 
         for (size_t dim = 0; dim != loads_.size(); ++dim)
             loads_[dim] = LoadSegment::merge(loads_[dim], segment.load(dim));
 
-        back_ = segment.back();
-        size_ += segment.size();
-        numClients_ += segment.numClients();
-        numPickups_ += segment.numPickups();
+        end++;
     }
 };
 
@@ -358,15 +346,16 @@ bool Solution::insert(Route::Node *pickup,
                 bestCost = deltaCost;
             }
 
-            SegmentAccumulator middle(data_, *route, route->at(V->pos() + 1));
-            for (auto const *node = n(V); !node->isDepot(); node = n(node))
+            IncrementalSegmentBetween between(data_, route, V->pos() + 1);
+            for (auto const *node = n(V); !node->isDepot();
+                 node = n(node), between.advance())
             {
                 Cost deltaCost = -shipment.prize;
                 costEvaluator.deltaCost<true>(
                     deltaCost,
                     Route::Proposal(route->before(V->pos()),
                                     PickupSegment(data_, pickup->idx()),
-                                    middle,
+                                    between,
                                     DeliverySegment(data_, delivery->idx()),
                                     route->after(node->pos() + 1)));
 
@@ -376,8 +365,6 @@ bool Solution::insert(Route::Node *pickup,
                     deliveryPos = node->pos() + 1;
                     bestCost = deltaCost;
                 }
-
-                middle.merge(route->at(node->pos() + 1));
             }
         }
     }
